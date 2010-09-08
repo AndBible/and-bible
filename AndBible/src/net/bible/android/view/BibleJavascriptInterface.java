bbeb706acf1caafad1cd03f37704e866c5c1a8de
Martin Denham <mjdenham@gmail.com>
2010-09-08 17:45:46 +0000

    Finish Verse calculation via js

--- AndBible/src/net/bible/android/view/BibleJavascriptInterface.java
+++ AndBible/src/net/bible/android/view/BibleJavascriptInterface.java
@@ -1,28 +1,31 @@
 package net.bible.android.view;
 
+import android.util.Log;
+
 public class BibleJavascriptInterface {
 
 	private VerseCalculator verseCalculator;
+	
+	private static final String TAG = "BibleJavascriptInterface";
 	
 	public BibleJavascriptInterface(VerseCalculator verseCalculator) {
 		this.verseCalculator = verseCalculator;
 	}
 	
-	public void onLoad(int scrollHeight) {
-		System.out.println("*onLoad from js - scrollht="+scrollHeight+" ******");
-		verseCalculator.init(scrollHeight);
+	public void onLoad() {
+		Log.d(TAG, "onLoad from js");
+		verseCalculator.init();
 	}
 
 	public void onScroll(int newYPos) {
-		System.out.println("*onScroll from js - y="+newYPos+" ******");
 		verseCalculator.newPosition(newYPos);
 	}
 	
 	public void registerVersePosition(int verse, int offset) {
-		System.out.println("******** Verse:"+verse+" offset:"+offset);
+		verseCalculator.registerVersePosition(verse, offset);
 	}
 	
 	public void log(String msg) {
-		System.out.println("***"+msg);
+		Log.d(TAG, msg);
 	}
 }
