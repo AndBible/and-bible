f0f55127b3a2cb1525e80b2470db02ca5b7fa3a5
Martin Denham <mjdenham@gmail.com>
2010-10-23 10:46:47 +0000

    adjusr help and splash layout

--- src/net/bible/android/activity/Help.java
+++ src/net/bible/android/activity/Help.java
@@ -1,19 +1,8 @@
 package net.bible.android.activity;
 
- import java.util.ArrayList;
-import java.util.List;
-
 import net.bible.android.activity.base.ActivityBase;
-import net.bible.service.history.HistoryItem;
-import net.bible.service.history.HistoryManager;
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
-import android.widget.ListView;
 
 /** show a history list and allow to go to history item
  * 
@@ -30,9 +19,5 @@ public class Help extends ActivityBase {
         super.onCreate(savedInstanceState);
         Log.i(TAG, "Displaying History view");
         setContentView(R.layout.help);
-    }
-    
-    public void onOkay(View v) {
-    	finish();
     }
 }
