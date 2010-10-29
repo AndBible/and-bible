8fc12046c3b2c70e9df309531aa69d8f01e3e06c
Martin Denham <mjdenham@gmail.com>
2010-10-29 13:07:06 +0000

    Download indexes and use actual size in percentage downloaded bar

--- src/net/bible/android/activity/ChooseDocument.java
+++ src/net/bible/android/activity/ChooseDocument.java
@@ -3,7 +3,7 @@ package net.bible.android.activity;
 import java.util.ArrayList;
 import java.util.List;
 
-import net.bible.android.activity.base.ActivityBase;
+import net.bible.android.activity.base.ListActivityBase;
 import net.bible.android.control.page.CurrentPage;
 import net.bible.android.control.page.CurrentPageManager;
 import net.bible.service.sword.SwordApi;
@@ -15,8 +15,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -27,11 +25,9 @@ import android.widget.ListView;
  * @see gnu.lgpl.License for license details.<br>
  *      The copyright to this program is held by it's author.
  */
-public class ChooseDocument extends ActivityBase {
+public class ChooseDocument extends ListActivityBase {
 	private static final String TAG = "ChooseDocument";
 	
-	private ListView bookList;
-
 	private List<Book> documents;
 
 	private static final int LIST_ITEM_TYPE = android.R.layout.simple_list_item_1; 
@@ -45,7 +41,6 @@ public class ChooseDocument extends ActivityBase {
     }
 
     private void initialiseView() {
-    	bookList = (ListView)findViewById(R.id.bookList);
     	documents = SwordApi.getInstance().getDocuments();
     	
     	// get long book names to show in the select list
@@ -57,18 +52,14 @@ public class ChooseDocument extends ActivityBase {
     	ArrayAdapter<String> listArrayAdapter = new ArrayAdapter<String>(this,
     	        LIST_ITEM_TYPE,
     	        docNames);
-    	bookList.setAdapter(listArrayAdapter);
-    	
-    	{
-	    	bookList.setOnItemClickListener(new OnItemClickListener() {
-	    	    @Override
-	    	    public void onItemClick(AdapterView<?> parentView, View selectedItemView, int position, long id) {
-	    	    	bookSelected(documents.get(position));
-	    	    }
-	    	});
-    	}
+    	setListAdapter(listArrayAdapter);
     }
-    
+
+    @Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+    	bookSelected(documents.get(position));
+    }
+
     private void bookSelected(Book selectedBook) {
     	Log.d(TAG, "Book selected:"+selectedBook.getInitials());
     	try {
