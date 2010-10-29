eeb8098dfefb2c9772544fbd7593d5101e80538b
Martin Denham <mjdenham@gmail.com>
2010-10-29 14:23:34 +0000

    limit length of text in result lists

--- src/net/bible/service/common/Utils.java
+++ src/net/bible/service/common/Utils.java
@@ -2,6 +2,9 @@ package net.bible.service.common;
 
 public class Utils {
 
+	private static final int DEFAULT_MAX_TEXT_LENGTH = 250;
+	private static final String ELLIPSIS = "...";
+
 	static private boolean isAndroid;
 	
 	//todo have to finish implementing switchable logging here
@@ -17,5 +20,21 @@ public class Utils {
 
 	public static boolean isAndroid() {
 		return isAndroid;
+	}
+
+	/** shorten text for display in lists etc.
+	 * 
+	 * @param text
+	 * @return
+	 */
+	public static String limitTextLength(String text) {
+		if (text!=null && text.length()>DEFAULT_MAX_TEXT_LENGTH) {
+			// break on a space rather than mid-word
+			int cutPoint = text.indexOf(" ", DEFAULT_MAX_TEXT_LENGTH);
+			if (cutPoint >= DEFAULT_MAX_TEXT_LENGTH) {
+				text = text.substring(0, cutPoint+1)+ELLIPSIS;
+			}
+		}
+		return text;
 	}
 }
