02554d74a8239b34ab50a96566d0aad8606e5b24
Martin Denham <mjdenham@gmail.com>
2010-11-02 15:00:38 +0000

    Title bar short cuts

--- src/net/bible/android/control/page/CurrentPageManager.java
+++ src/net/bible/android/control/page/CurrentPageManager.java
@@ -1,6 +1,9 @@
 package net.bible.android.control.page;
 
+import java.util.List;
+
 import net.bible.android.control.PassageChangeMediator;
+import net.bible.service.sword.SwordApi;
 
 import org.crosswire.jsword.book.Book;
 import org.crosswire.jsword.book.BookCategory;
@@ -119,5 +122,48 @@ public class CurrentPageManager {
 		PassageChangeMediator.getInstance().onBeforeCurrentPageChanged();
 		currentDisplayedPage = currentBiblePage;
 		PassageChangeMediator.getInstance().onCurrentPageChanged();
+	}
+	
+	/** Suggest an alternative bible to view or return null
+	 * 
+	 * @return
+	 */
+	public Book getSuggestedBible() {
+		Book currentBible = currentBiblePage.getCurrentDocument();
+		return getSuggestedBook(SwordApi.getInstance().getBibles(), currentBible, isBibleShown());
+	}
+
+	/** Suggest an alternative comentary to view or return null
+	 * 
+	 * @return
+	 */
+	public Book getSuggestedCommentary() {
+		Book currentCommentary = currentCommentaryPage.getCurrentDocument();
+		return getSuggestedBook(SwordApi.getInstance().getBooks(BookCategory.COMMENTARY), currentCommentary,  currentCommentaryPage == currentDisplayedPage);
+	}
+	
+	/** Suggest an alternative document to view or return null
+	 * 
+	 * @return
+	 */
+	private Book getSuggestedBook(List<Book> books, Book currentBook, boolean isShownNow) {
+		Book suggestion = null;
+		if (!isShownNow) {
+			// allow easy switch back to bible view
+			suggestion = currentBook;
+		} else {
+			// only suggest alternative if more than 1
+			if (books.size()>1) {
+				for (int i=0; i<books.size() && suggestion==null; i++) {
+					Book bible = books.get(i);
+					if (bible.equals(currentBook)) {
+						// next bible in list or wrap around to first bible
+						suggestion = books.get((i+1)%books.size()); 
+					}
+				}
+			}
+		}
+		
+		return suggestion;
 	}
 }
