f2bd35564afdd89899f70b4ba5d8bbc7fbce1cff
Martin Denham <mjdenham@gmail.com>
2010-11-18 13:57:54 +0000

    Limit suggested documents to those with content in current bible book

--- src/net/bible/android/control/document/DocumentControl.java
+++ src/net/bible/android/control/document/DocumentControl.java
@@ -8,8 +8,9 @@ import net.bible.service.sword.SwordApi;
 
 import org.crosswire.jsword.book.Book;
 import org.crosswire.jsword.book.BookCategory;
-
-import android.view.View;
+import org.crosswire.jsword.passage.Key;
+import org.crosswire.jsword.passage.KeyUtil;
+import org.crosswire.jsword.passage.Verse;
 
 public class DocumentControl {
 	public boolean canDelete(Book document) {
@@ -25,36 +26,54 @@ public class DocumentControl {
 	public Book getSuggestedBible() {
 		CurrentPageManager currentPageManager = ControlFactory.getInstance().getCurrentPageControl();
 		Book currentBible = currentPageManager.getCurrentBible().getCurrentDocument();
-		return getSuggestedBook(SwordApi.getInstance().getBibles(), currentBible, currentPageManager.isBibleShown());
+		Key requiredVerse = getRequiredVerseForSuggestions();
+		return getSuggestedBook(SwordApi.getInstance().getBibles(), currentBible, requiredVerse, currentPageManager.isBibleShown());
 	}
 
-	/** Suggest an alternative comentary to view or return null
+	/** Suggest an alternative commentary to view or return null
 	 * 
 	 * @return
 	 */
 	public Book getSuggestedCommentary() {
 		CurrentPageManager currentPageManager = ControlFactory.getInstance().getCurrentPageControl();
 		Book currentCommentary = currentPageManager.getCurrentCommentary().getCurrentDocument();
-		return getSuggestedBook(SwordApi.getInstance().getBooks(BookCategory.COMMENTARY), currentCommentary,  currentPageManager.isCommentaryShown());
+		Key requiredVerse = getRequiredVerseForSuggestions();
+		return getSuggestedBook(SwordApi.getInstance().getBooks(BookCategory.COMMENTARY), currentCommentary, requiredVerse, currentPageManager.isCommentaryShown());
 	}
 	
+	/** possible books will often not include the current verse but most will include chap 1 verse 1
+	 */
+	private Key getRequiredVerseForSuggestions() {
+		Key currentVerseKey = ControlFactory.getInstance().getCurrentPageControl().getCurrentBible().getSingleKey();
+		Verse verse = KeyUtil.getVerse(currentVerseKey);
+		return new Verse(verse.getBook(), 1, 1, true);
+	}
+
 	/** Suggest an alternative document to view or return null
 	 * 
 	 * @return
 	 */
-	private Book getSuggestedBook(List<Book> books, Book currentBook, boolean isShownNow) {
+	private Book getSuggestedBook(List<Book> books, Book currentDocument, Key requiredKey, boolean isBookTypeShownNow) {
 		Book suggestion = null;
-		if (!isShownNow) {
+		if (!isBookTypeShownNow) {
 			// allow easy switch back to bible view
-			suggestion = currentBook;
+			suggestion = currentDocument;
 		} else {
 			// only suggest alternative if more than 1
 			if (books.size()>1) {
-				for (int i=0; i<books.size() && suggestion==null; i++) {
-					Book bible = books.get(i);
-					if (bible.equals(currentBook)) {
-						// next bible in list or wrap around to first bible
-						suggestion = books.get((i+1)%books.size()); 
+				// find index of current document
+				int currentDocIndex = -1;
+				for (int i=0; i<books.size(); i++) {
+					if (books.get(i).equals(currentDocument)) {
+						currentDocIndex = i;
+					}
+				}
+				
+				// find the next doc containing related content e.g. if in NT then don't show TDavid
+				for (int i=0; i<books.size()-1 && suggestion==null; i++) {
+					Book possibleDoc = books.get((currentDocIndex+i+1)%books.size());
+					if (possibleDoc.contains(requiredKey)) {
+						 suggestion = possibleDoc;
 					}
 				}
 			}
