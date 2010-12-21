3fddea580d8aeeafad020747a5d3d70c6b6a6d46
Martin Denham <mjdenham@gmail.com>
2010-12-21 22:37:58 +0000

    decrease timeout from over inflated value to largish value - 8 secs

--- src/net/bible/service/sword/SwordApi.java
+++ src/net/bible/service/sword/SwordApi.java
@@ -109,7 +109,7 @@ public class SwordApi {
 		        // the following causes Sword to initialise itself and can take quite a few seconds
 				SwordBookPath.setAugmentPath(new File[] {SharedConstants.MANUAL_INSTALL_DIR});  // add manual install dir to this list
 				
-				WebResource.setTimeout(20000);
+				WebResource.setTimeout(8000);
 				
 				// because the above line causes initialisation set the is initialised flag here
 				isSwordLoaded = true;
