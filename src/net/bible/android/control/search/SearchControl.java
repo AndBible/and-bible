b9e880f63ced63140b6ef9d8f54936176c4bd191
Martin Denham <mjdenham@gmail.com>
2010-12-22 12:31:34 +0000

    Standardise structure of Search module

--- src/net/bible/android/control/search/SearchControl.java
+++ src/net/bible/android/control/search/SearchControl.java
@@ -1,20 +1,65 @@
 package net.bible.android.control.search;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import net.bible.android.activity.R;
 import net.bible.android.control.page.CurrentPageManager;
 import net.bible.android.view.activity.base.Dialogs;
-import net.bible.android.view.activity.search.SearchIndexProgressStatus;
+import net.bible.android.view.activity.search.Search;
 import net.bible.service.common.CommonUtils;
+import net.bible.service.db.bookmark.BookmarkDto;
 import net.bible.service.sword.SwordApi;
 
 import org.crosswire.jsword.book.Book;
+import org.crosswire.jsword.book.BookException;
+import org.crosswire.jsword.passage.Key;
 
-import android.content.Intent;
 import android.util.Log;
+import android.widget.Toast;
 
 public class SearchControl {
 
+	public static final int MAX_SEARCH_RESULTS = 100;
+
 	private static final String TAG = "SearchControl";
+	
+    /** do the search query and prepare results in lists ready for display
+     * 
+     */
+    public List<Key> getSearchResults(String searchText) throws BookException {
+    	Log.d(TAG, "Preparing search results");
+    	List<Key> resultKeys = new ArrayList<Key>();
+    	
+    	// search the current book
+        Book book = CurrentPageManager.getInstance().getCurrentPage().getCurrentDocument();
+    	SwordApi swordApi = SwordApi.getInstance();
+    	Key result = swordApi.search(book, searchText);
+    	if (result!=null) {
+    		int resNum = result.getCardinality();
+        	Log.d(TAG, "Number of results:"+resNum);
+
+    		for (int i=0; i<Math.min(resNum, MAX_SEARCH_RESULTS+1); i++) {
+    			resultKeys.add(result.get(i));
+    		}
+    	}
+    	
+    	return resultKeys;
+    }
+
+    /** get the verse for a search result
+     */
+	public String getSearchResultVerseText(Key key) {
+		// There is similar functionality in BookmarkControl
+		String verseText = "";
+		try {
+			verseText = SwordApi.getInstance().getPlainText(CurrentPageManager.getInstance().getCurrentBible().getCurrentDocument(), key.getOsisRef(), 1);
+			verseText = CommonUtils.limitTextLength(verseText);
+		} catch (Exception e) {
+			Log.e(TAG, "Error getting verse text", e);
+		}
+		return verseText;
+	}
 	
 	/** download index 
 	 * 
