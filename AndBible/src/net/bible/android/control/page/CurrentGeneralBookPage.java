1e8392640a926b859c3fb624dc6d69e8472f9d98
Martin Denham <mjdenham@gmail.com>
2011-03-12 15:53:37 +0000

    fix error in JSword fix move prev in gen book

--- AndBible/src/net/bible/android/control/page/CurrentGeneralBookPage.java
+++ AndBible/src/net/bible/android/control/page/CurrentGeneralBookPage.java
@@ -5,6 +5,7 @@ import net.bible.android.view.activity.navigation.ChooseGeneralBookKey;
 
 import org.crosswire.jsword.book.BookCategory;
 import org.crosswire.jsword.passage.Key;
+import org.crosswire.jsword.passage.TreeKey;
 
 import android.util.Log;
 import android.view.Menu;
@@ -53,29 +54,85 @@ public class CurrentGeneralBookPage extends CurrentPageBase implements CurrentPa
 
 	@Override
 	public void next() {
-		setKey(getKeyPlus(1));
+		Key next = getKeyPlus(1);
+		if (next!=null) {
+			setKey(next);
+		}
 	}
 
 	@Override
 	public void previous() {
-		setKey(getKeyPlus(-1));
+		Key prev = getKeyPlus(-1);
+		if (prev!=null) {
+			setKey(prev);
+		}
 	}
 
-	/** add or subtract a number of pages from the current position and return Verse
+	/** add or subtract a number of pages from the current position and return new key or null if run off end
 	 */
 	public Key getKeyPlus(int num) {
-		Key currentKey = getKey();
-		Key globalList = getCurrentDocument().getGlobalKeyList();
-		int keyPos = globalList.indexOf(currentKey);
-		Log.d(TAG, "current key pos:"+keyPos);
-		// move forward or backward to new posn
-		int newKeyPos = keyPos+num;
-		// check bounds
-		newKeyPos = Math.min(newKeyPos, globalList.getCardinality()-1);
-		newKeyPos = Math.max(newKeyPos, 0);
-		Log.d(TAG, "new key pos:"+newKeyPos);
-		// get the actual key at that posn
-		return globalList.get(newKeyPos);
+		Key key = getKey();
+		// GenBooks normally have TreeKeys - if it isn't then prevent a crash by bailing out now
+		if (!(key instanceof TreeKey)) {
+			return key;
+		}
+
+		TreeKey treeKey = (TreeKey)key;
+		for (int i=0; i<Math.abs(num); i++) {
+			if (num>0) {
+				treeKey = getNextSibling(treeKey);
+			} else {
+				treeKey = getPrevSibling(treeKey);
+			}
+		}
+		return treeKey;
+	}
+
+	/** move forward one place in a KeyTree
+	 * 
+	 * @param treeKey
+	 * @return
+	 */
+	private TreeKey getNextSibling(TreeKey treeKey) {
+		TreeKey sibling = null;
+		Key parent = treeKey.getParent();
+		if (parent!=null) {
+			int indexOfKey = parent.indexOf(treeKey);
+			if (indexOfKey < parent.getChildCount()-1) {
+				sibling = (TreeKey)parent.get(indexOfKey+1);
+			} else if (parent instanceof TreeKey) {
+				sibling = getNextSibling((TreeKey)parent);
+			}
+
+			if (sibling!=null) {
+				// ensure we always end at a leaf node
+				while (sibling.getChildCount()>0) {
+					sibling = (TreeKey)sibling.get(0);
+				}
+			}
+		}
+		return sibling;
+	}
+
+	private TreeKey getPrevSibling(TreeKey treeKey) {
+		TreeKey sibling = null;
+		Key parent = treeKey.getParent();
+		if (parent!=null) {
+			int indexOfKey = parent.indexOf(treeKey);
+			if (indexOfKey > 0) {
+				sibling = (TreeKey)parent.get(indexOfKey-1);
+			} else if (parent instanceof TreeKey) {
+				sibling = getPrevSibling((TreeKey)parent);
+			}
+
+			if (sibling!=null) {
+				// ensure we always end at a leaf node
+				while (sibling.getChildCount()>0) {
+					sibling = (TreeKey)sibling.get(sibling.getChildCount()-1);
+				}
+			}
+		}
+		return sibling;
 	}
 	
 	@Override
