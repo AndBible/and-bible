97d102cdb1692865b98156061d04bc3dca16c3b8
Martin Denham <mjdenham@gmail.com>
2011-11-12 19:55:57 +0000

    Fix document parse method calculation

--- AndBible/src/net/bible/service/sword/DocumentParseMethod.java
+++ AndBible/src/net/bible/service/sword/DocumentParseMethod.java
@@ -7,6 +7,10 @@ import org.crosswire.jsword.book.Book;
 import org.crosswire.jsword.passage.Key;
 import org.crosswire.jsword.passage.KeyUtil;
 import org.crosswire.jsword.passage.Verse;
+import org.crosswire.jsword.versification.BibleBook;
+import org.crosswire.jsword.versification.BibleInfo;
+
+import android.util.Log;
 
 /** Record which documents have bad xml, normally in first or last chapters and use slightly slower JSword parser with error recovery
  *		we have a fast way of handling OSIS zText docs but the following need the superior JSword error recovery for mismatching tags 
@@ -30,26 +34,20 @@ public class DocumentParseMethod {
 								FIRST_AND_LAST_BOOK_CHAPTER, 
 								ALL}
 	
-	private class DocumentFailInfo {
-		String documentInitials;
-		FailPosition failPosition;
-	}
+	private Map<String, FailPosition> failureInfoMap = new HashMap<String, FailPosition>();
 	
-	private Map<String, DocumentFailInfo> failureInfoMap = new HashMap<String, DocumentFailInfo>();
-	
-	/*
-	 */
+	private static final String TAG = "DocumentParseMethod";
 	
 	/** return true if this book's chapter is believed to have a good xml structure and not require recovery fallback
 	 */
 	public boolean isFastParseOkay(Book document, Key key) {
 		boolean isFastParseOkay = false;
-		DocumentFailInfo documentFailInfo = failureInfoMap.get(document.getInitials());
-		if (documentFailInfo==null) {
+		FailPosition documentFailPosition = failureInfoMap.get(document.getInitials());
+		if (documentFailPosition==null) {
 			isFastParseOkay = true;
 		} else {
 			
-			switch (documentFailInfo.failPosition) {
+			switch (documentFailPosition) {
 			case NONE:
 				// should never come here
 				isFastParseOkay = true;
@@ -58,8 +56,7 @@ public class DocumentParseMethod {
 				isFastParseOkay = false;
 				break;
 			case FIRST_AND_LAST_BOOK_CHAPTER:
-				Verse verse = KeyUtil.getVerse(key);
-				isFastParseOkay = !(verse.isStartOfBook() || verse.isEndOfBook());
+				isFastParseOkay = !isStartOrEndOfBook(key);
 				break;
 			}
 		}
@@ -70,20 +67,30 @@ public class DocumentParseMethod {
 	 * many books have extra tags in first and/or last chapters hence the graded level of failures
 	 */
 	public void failedToParse(Book document, Key key) {
-		DocumentFailInfo documentFailInfo = failureInfoMap.get(document.getInitials());
-		if (documentFailInfo==null) {
-			documentFailInfo = new DocumentFailInfo();
-		}
-		
-		documentFailInfo.documentInitials = document.getInitials();
+		String initials = document.getInitials();
+		FailPosition documentFailPosition = failureInfoMap.get(initials);
 
-		Verse verse = KeyUtil.getVerse(key);
-		if (verse.isStartOfBook() || verse.isEndOfBook()) {
-			documentFailInfo.failPosition = FailPosition.FIRST_AND_LAST_BOOK_CHAPTER;
+		if (isStartOrEndOfBook(key)) {
+			documentFailPosition = FailPosition.FIRST_AND_LAST_BOOK_CHAPTER;
 		} else {
-			documentFailInfo.failPosition = FailPosition.ALL;
+			documentFailPosition = FailPosition.ALL;
 		}
 		
-		failureInfoMap.put(documentFailInfo.documentInitials, documentFailInfo);
+		failureInfoMap.put(initials, documentFailPosition);
+	}
+	
+	private boolean isStartOrEndOfBook(Key key) {
+		boolean isStartOrEnd = false;
+		try {
+			Verse verse = KeyUtil.getVerse(key);
+			
+			int chapter = verse.getChapter();
+			BibleBook book = verse.getBook();
+			isStartOrEnd = 	chapter == 1 || chapter == BibleInfo.chaptersInBook(book);
+		} catch (Exception e) {
+			Log.e(TAG, "Verse error", e);
+			isStartOrEnd = false;
+		}
+		return isStartOrEnd;
 	}
 }
