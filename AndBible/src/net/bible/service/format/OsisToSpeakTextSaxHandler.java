15e21bbe44c9f0cb8390740fa3bf7968734b6fa7
Martin Denham <mjdenham@gmail.com>
2011-12-06 21:11:47 +0000

    undo -- replacement change

--- AndBible/src/net/bible/service/format/OsisToSpeakTextSaxHandler.java
+++ AndBible/src/net/bible/service/format/OsisToSpeakTextSaxHandler.java
@@ -62,8 +62,8 @@ public class OsisToSpeakTextSaxHandler extends OsisToCanonicalTextSaxHandler {
     		s = s.replace("\'", "\"");
     	}
     	// Finney Gospel Sermons contains to many '--'s which are pronounced as hyphen hyphen
-    	if (s.contains("--")) {
-    		s = s.replaceAll("[^-]--", ";");
+    	if (s.contains(" --")) {
+    		s = s.replace(" --", ";");
     	}
 		
 		// say verse rather than colon etc.
