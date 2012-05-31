7e4c7c093582d6cbc4bd3c96c3f8207a36c4e03c
Martin Denham <mjdenham@gmail.com>
2012-05-31 18:50:01 +0100

    Make double sure TTS text is not > 4000 chars

--- AndBible/src/net/bible/android/device/SpeakTextProvider.java
+++ AndBible/src/net/bible/android/device/SpeakTextProvider.java
@@ -17,7 +17,12 @@ public class SpeakTextProvider {
 
     private List<String> mTextToSpeak = new ArrayList<String>();
     private int currentSentence = 0;
-
+    
+    // Before ICS Android would split up long text for you but since ICS this error occurs:
+	//    if (mText.length() >= MAX_SPEECH_ITEM_CHAR_LENGTH) {
+	//        Log.w(TAG, "Text too long: " + mText.length() + " chars");
+    private static final int MAX_SPEECH_ITEM_CHAR_LENGTH = 4000;
+    
     // require DOTALL to allow . to match new lines which occur in books like JOChrist
 	private static Pattern BREAK_PATTERN = Pattern.compile(".{100,3000}[a-z]+[.?!][\\s]{1,}+", Pattern.DOTALL);
 	
@@ -25,9 +30,7 @@ public class SpeakTextProvider {
 
 	public void addTextToSpeak(String textToSpeak) {
    		this.mTextToSpeak.addAll(breakUpText(textToSpeak));
-
     	Log.d(TAG, "Num sentences:"+mTextToSpeak.size());
-
 	}
 	
 	public boolean isMoreTextToSpeak() {
@@ -46,22 +49,52 @@ public class SpeakTextProvider {
 		currentSentence = 0;
 	}
 
+	/** ICS rejects text longer than 4000 chars so break it up
+	 * 
+	 */
 	private List<String> breakUpText(String text) {
-		List<String> chunks = new ArrayList<String>();
+		//
+		// first try to split text nicely at the end of sentences
+		//
+		List<String> chunks1 = new ArrayList<String>();
 		Matcher matcher = BREAK_PATTERN.matcher(text);
 
 		int matchedUpTo = 0;
 		while (matcher.find()) {
 			int nextEnd = matcher.end();
-			chunks.add(text.substring(matchedUpTo, nextEnd));
+			chunks1.add(text.substring(matchedUpTo, nextEnd));
 			matchedUpTo = nextEnd;
 		}
 		// add on the final part of the text
-		chunks.add(text.substring(matchedUpTo));
+		chunks1.add(text.substring(matchedUpTo));
+		
+		//
+		// If any text is still too long because the regexp was not matched then forcefully split it up
+		// All chunks are probably now less than 4000 chars as required by tts but go through again for languages that don't have '. ' at the end of sentences
+		//
+		List<String> chunks2 = new ArrayList<String>();
+		for (String chunk : chunks1) {
+			if (chunk.length()<MAX_SPEECH_ITEM_CHAR_LENGTH) {
+				chunks2.add(chunk);
+			} else {
+				// force chunks to be correct length -10 is just to allow a bit of extra room
+				chunks2.addAll(splitEqually(chunk, MAX_SPEECH_ITEM_CHAR_LENGTH-10));
+			}
+		}
 
-		return chunks;
+		return chunks2;
 	}
+	
+	public static List<String> splitEqually(String text, int size) {
+	    // Give the list the right capacity to start with. You could use an array
+	    // instead if you wanted.
+	    List<String> ret = new ArrayList<String>((text.length() + size - 1) / size);
 
+	    for (int start = 0; start < text.length(); start += size) {
+	        ret.add(text.substring(start, Math.min(text.length(), start + size)));
+	    }
+	    return ret;
+	}
 //	private List<String> nonREbreakUpText(String text) {
 //		List<String> chunks = new ArrayList<String>();
 //
