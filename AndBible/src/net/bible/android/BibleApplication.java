85d571bd0dbc5de5c63cf054060a7e527e711de9
Martin Denham <mjdenham@gmail.com>
2012-08-15 18:30:17 +0100

    ICS Locale change fix

--- AndBible/src/net/bible/android/BibleApplication.java
+++ AndBible/src/net/bible/android/BibleApplication.java
@@ -93,9 +93,12 @@ public class BibleApplication extends Application{
         super.onConfigurationChanged(newConfig);
         if (overrideLocale != null)
         {
-            newConfig.locale = overrideLocale;
-            Locale.setDefault(overrideLocale);
-            getBaseContext().getResources().updateConfiguration(newConfig, getBaseContext().getResources().getDisplayMetrics());
+        	if (!overrideLocale.getLanguage().equals(newConfig.locale.getLanguage())) {
+            	Log.d(TAG, "re-applying changed Locale");
+	            newConfig.locale = overrideLocale;
+	            Locale.setDefault(overrideLocale);
+	            getBaseContext().getResources().updateConfiguration(newConfig, getBaseContext().getResources().getDisplayMetrics());
+        	}
         }
     }
 
