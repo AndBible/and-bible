daa078c0b63541bf4965db867769b93794d6c62d
Martin Denham <mjdenham@gmail.com>
2012-11-13 12:19:49 +0000

    update localisations from Transifex - new ar, hi, ml; updated ko, lv, th

--- AndBible/txFetch.bat
+++ AndBible/txFetch.bat
@@ -1,2 +1,5 @@
+rem tx status // to view
+rem tx pull --force // force download of translations ignoring file dates
+rem tx push -s // push the English source file to Transifex 
 tx pull
 copy res\values-he\strings.xml res\values-iw
