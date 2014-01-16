7216cc56299e526247807efbec4ce73cd88e8756
mjdenham <mjdenham@gmail.com>
2014-01-16 23:19:13 +0000

    revert to AB v11n mapping code because of problems with new

--- AndBible/src/net/bible/android/control/versification/VersificationConverter.java
+++ AndBible/src/net/bible/android/control/versification/VersificationConverter.java
@@ -1,10 +1,10 @@
 package net.bible.android.control.versification;
 
-import org.crosswire.jsword.passage.Key;
-import org.crosswire.jsword.passage.KeyUtil;
+import net.bible.android.control.versification.mapping.VersificationMapping;
+import net.bible.android.control.versification.mapping.VersificationMappingFactory;
+
 import org.crosswire.jsword.passage.Verse;
 import org.crosswire.jsword.versification.Versification;
-import org.crosswire.jsword.versification.VersificationsMapper;
 
 /** Manage conversion of verses to a specific versification
  * 
@@ -13,12 +13,19 @@ import org.crosswire.jsword.versification.VersificationsMapper;
  *      The copyright to this program is held by it's author.
  */
 public class VersificationConverter {
-	private VersificationsMapper versificationsMapper = VersificationsMapper.instance();
+	private VersificationMappingFactory versificationMappingFactory = VersificationMappingFactory.getInstance();
 
 	/** Return the verse in the required versification, mapping if necessary
 	 */
 	public Verse convert(Verse verse, Versification toVersification) {
-		Key key = versificationsMapper.mapVerse(verse, toVersification);
-		return KeyUtil.getVerse(key);
+//		Key key = versificationsMapper.mapVerse(verse, toVersification);
+//		return KeyUtil.getVerse(key);
+		if (toVersification.equals(verse.getVersification())) {
+			return verse;
+		} else {
+			VersificationMapping versificationMapping = versificationMappingFactory.getVersificationMapping(verse.getVersification(), toVersification);
+			return versificationMapping.getMappedVerse(verse, toVersification);
+		}
+
 	}
 }
