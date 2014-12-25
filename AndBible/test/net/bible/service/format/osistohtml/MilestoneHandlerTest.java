b106bd39a019c4cf9f61a48e58e3aebc36ad1dab
mjdenham <mjdenham@gmail.com>
2014-12-25 22:37:17 +0000

    Start to consider milestone handling

---
+++ AndBible/test/net/bible/service/format/osistohtml/MilestoneHandlerTest.java
@@ -0,0 +1,20 @@
+package net.bible.service.format.osistohtml;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MilestoneHandlerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * milestone marker='"' at start of verse
+	 * diatheke -b ESV -f OSIS -k Jn 3:16
+	 */
+	@Test
+	public void test() {
+	}
+
+}
