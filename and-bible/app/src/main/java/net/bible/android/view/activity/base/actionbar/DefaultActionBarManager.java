4b3b75938bfcce0576b2fa59c2c9d353a41f89af
mjdenham <mjdenham@gmail.com>
2016-04-30 19:18:17 +0100

    Restyle ok in Day mode

--- and-bible/app/src/main/java/net/bible/android/view/activity/base/actionbar/DefaultActionBarManager.java
+++ and-bible/app/src/main/java/net/bible/android/view/activity/base/actionbar/DefaultActionBarManager.java
@@ -1,14 +1,10 @@
 package net.bible.android.view.activity.base.actionbar;
 
-import net.bible.android.activity.R;
-import net.bible.android.view.activity.base.CurrentActivityHolder;
-import net.bible.service.common.CommonUtils;
-import net.bible.service.device.ScreenSettings;
 import android.app.Activity;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
 import android.support.v7.app.ActionBar;
 import android.view.Menu;
+
+import net.bible.android.view.util.UiUtils;
 
 /**
  * @author Martin Denham [mjdenham at gmail dot com]
@@ -19,39 +15,17 @@ public class DefaultActionBarManager implements ActionBarManager {
 
 	private ActionBar actionBar;
 	
-	private static final int night = CommonUtils.getResourceColor(R.color.actionbar_background_night);
-	private static final int day = CommonUtils.getResourceColor(R.color.actionbar_background_day);
-	
 	@Override
 	public void prepareOptionsMenu(Activity activity, Menu menu, ActionBar actionBar) {
 		this.actionBar = actionBar;
 		
-		setActionBarColor();
+		UiUtils.setActionBarColor(actionBar);
 		
 		actionBar.setDisplayShowHomeEnabled(false);
 	}
 
 	@Override
 	public void updateButtons() {
-		setActionBarColor();
+		UiUtils.setActionBarColor(actionBar);
 	}
-	
-	/** change actionBar colour according to day/night state
-	 */
-	protected void setActionBarColor() {
-		changeColor(ScreenSettings.isNightMode()? night : day, actionBar);
-	}
-
-    private void changeColor(final int newColor, final ActionBar actionBar) {
-    	if (actionBar!=null) {
-    		CurrentActivityHolder.getInstance().runOnUiThread(new Runnable() {
-
-				@Override
-				public void run() {
-			        Drawable colorDrawable = new ColorDrawable(newColor);
-			        actionBar.setBackgroundDrawable(colorDrawable);
-			    }
-    		});
-    	}
-    }
 }
--- and-bible/app/src/main/java/net/bible/android/view/util/UiUtils.java
+++ and-bible/app/src/main/java/net/bible/android/view/util/UiUtils.java
@@ -1,8 +1,14 @@
 package net.bible.android.view.util;
 
-import net.bible.android.activity.R;
-import net.bible.service.device.ScreenSettings;
 import android.app.Activity;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.ActionBar;
+
+import net.bible.android.activity.R;
+import net.bible.android.view.activity.base.CurrentActivityHolder;
+import net.bible.service.common.CommonUtils;
+import net.bible.service.device.ScreenSettings;
 
 /**
  * @author Martin Denham [mjdenham at gmail dot com]
@@ -11,9 +17,9 @@ import android.app.Activity;
  */
 public class UiUtils {
 
-	@SuppressWarnings("unused")
-	private static final String TAG = "UiUtils"; 
-	
+	private static final int night = CommonUtils.getResourceColor(R.color.actionbar_background_night);
+	private static final int day = CommonUtils.getResourceColor(R.color.actionbar_background_day);
+
 
     public static void applyTheme(Activity activity) {
     	ScreenSettings.isNightModeChanged();
@@ -23,4 +29,22 @@ public class UiUtils {
         	activity.setTheme(R.style.AppThemeDay);
         }
     }
+
+	/** Change actionBar colour according to day/night state
+	 */
+	public static void setActionBarColor(final ActionBar actionBar) {
+		final int newColor = ScreenSettings.isNightMode() ? night : day;
+
+		if (actionBar!=null) {
+			CurrentActivityHolder.getInstance().runOnUiThread(new Runnable() {
+
+				@Override
+				public void run() {
+					Drawable colorDrawable = new ColorDrawable(newColor);
+			        actionBar.setBackgroundDrawable(colorDrawable);
+				}
+			});
+		}
+	}
+
 }
