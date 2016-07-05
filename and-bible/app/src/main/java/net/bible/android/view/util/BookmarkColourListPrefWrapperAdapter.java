944ebef2e06f3fca75c0cca4f04072c150225a9d
mjdenham <mjdenham@gmail.com>
2016-07-05 21:01:17 +0100

    Simplify textview with image control

--- and-bible/app/src/main/java/net/bible/android/view/util/BookmarkColourListPrefWrapperAdapter.java
+++ and-bible/app/src/main/java/net/bible/android/view/util/BookmarkColourListPrefWrapperAdapter.java
@@ -2,6 +2,8 @@ package net.bible.android.view.util;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.text.SpannableString;
+import android.text.style.ImageSpan;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,7 +12,6 @@ import android.widget.TextView;
 
 import net.bible.android.activity.R;
 import net.bible.android.control.bookmark.BookmarkStyle;
-import net.bible.android.view.util.widget.CheckedTextViewWithImages;
 import net.bible.android.view.util.widget.ListPrefWrapperAdapter;
 import net.bible.service.common.CommonUtils;
 
@@ -34,34 +35,47 @@ class BookmarkColourListPrefWrapperAdapter extends ListPrefWrapperAdapter {
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		TextView view = new CheckedTextViewWithImages(context);
-		String text = sampleText;
+		TextView view = (TextView)super.getView(position, convertView, parent);
 		int backgroundColor = Color.WHITE;
 
 		switch (BookmarkStyle.values()[position]) {
 			case YELLOW_STAR:
 				backgroundColor = BookmarkStyle.YELLOW_STAR.getBackgroundColor();
-				text = "[img src=goldstar16x16/]"+text;
+				CharSequence imgText = addImageAtStart("* "+sampleText, R.drawable.goldstar16x16);
+				view.setText(imgText, TextView.BufferType.SPANNABLE);
 				break;
 			case RED_HIGHLIGHT:
 				backgroundColor = BookmarkStyle.RED_HIGHLIGHT.getBackgroundColor();
+				view.setText(sampleText);
 				break;
 			case YELLOW_HIGHLIGHT:
 				backgroundColor = BookmarkStyle.YELLOW_HIGHLIGHT.getBackgroundColor();
+				view.setText(sampleText);
 				break;
 			case GREEN_HIGHLIGHT:
 				backgroundColor = BookmarkStyle.GREEN_HIGHLIGHT.getBackgroundColor();
+				view.setText(sampleText);
 				break;
 			case BLUE_HIGHLIGHT:
 				backgroundColor = BookmarkStyle.BLUE_HIGHLIGHT.getBackgroundColor();
+				view.setText(sampleText);
 				break;
 		}
 		view.setBackgroundColor(backgroundColor);
-		view.setText(text);
 
 		view.setTextColor(Color.BLACK);
 		view.setGravity(Gravity.CENTER);
 		view.setHeight(CommonUtils.convertDipsToPx(30));
 		return view;
+	}
+
+	/**
+	 * Replace first character of text with image
+	 */
+	private CharSequence addImageAtStart(String text, int drawableImage) {
+		ImageSpan imageSpan = new ImageSpan(context, drawableImage, ImageSpan.ALIGN_BASELINE);
+		final SpannableString spannableString = new SpannableString(text);
+		spannableString.setSpan(imageSpan, 0, 1, SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE);
+		return spannableString;
 	}
 }
