eb66ccad886e9562f0afb804522f797343fcd720
mjdenham <mjdenham@gmail.com>
2016-07-08 21:23:38 +0100

    Tidy

--- and-bible/app/src/main/java/net/bible/service/db/bookmark/BookmarkDBAdapter.java
+++ and-bible/app/src/main/java/net/bible/service/db/bookmark/BookmarkDBAdapter.java
@@ -16,9 +16,8 @@ import net.bible.service.db.bookmark.BookmarkDatabaseDefinition.LabelColumn;
 import net.bible.service.db.bookmark.BookmarkDatabaseDefinition.Table;
 
 import org.apache.commons.lang.StringUtils;
-import org.crosswire.jsword.passage.Key;
 import org.crosswire.jsword.passage.NoSuchKeyException;
-import org.crosswire.jsword.passage.VerseKey;
+import org.crosswire.jsword.passage.VerseRange;
 import org.crosswire.jsword.passage.VerseRangeFactory;
 import org.crosswire.jsword.versification.BibleBook;
 import org.crosswire.jsword.versification.Versification;
@@ -39,7 +38,7 @@ public class BookmarkDBAdapter {
 	private SQLiteDatabase db;
 
 	// Database open/upgrade helper
-	private SQLiteOpenHelper dbHelper;
+	private final SQLiteOpenHelper dbHelper;
 	
 	private static final String TAG = "BookmarkDBAdapter";
 
@@ -63,23 +62,19 @@ public class BookmarkDBAdapter {
 	public BookmarkDto insertBookmark(BookmarkDto bookmark) {
 		// Create a new row of values to insert.
 		ContentValues newValues = new ContentValues();
-		Key key = bookmark.getVerseRange();
-		String v11nName="";
-		if (key instanceof VerseKey) {
-			// must save a VerseKey's versification along with the key!
-			v11nName = ((VerseKey<?>) key).getVersification().getName();
-		}
-		
+		VerseRange key = bookmark.getVerseRange();
+		// must save a Key's versification along with the key!
+		String v11nName = key.getVersification().getName();
+
 		// Gets the current system time in milliseconds
-        Long now = Long.valueOf(System.currentTimeMillis());
-        
+        Long now = System.currentTimeMillis();
+
 		newValues.put(BookmarkColumn.KEY, key.getOsisRef());
 		newValues.put(BookmarkColumn.VERSIFICATION, v11nName);
 		newValues.put(BookmarkColumn.CREATED_ON, now);
 
 		long newId = db.insert(Table.BOOKMARK, null, newValues);
-		BookmarkDto newBookmark = getBookmarkDto(newId);
-		return newBookmark;
+		return getBookmarkDto(newId);
 	}
 
 	public boolean removeBookmark(BookmarkDto bookmark) {
@@ -98,8 +93,7 @@ public class BookmarkDBAdapter {
 		newValues.put(LabelColumn.NAME, label.getName());
 
 		long newId = db.insert(Table.LABEL, null, newValues);
-		LabelDto newLabel = getLabelDto(newId);
-		return newLabel;
+		return getLabelDto(newId);
 	}
 
 	public LabelDto updateLabel(LabelDto label) {
@@ -108,8 +102,7 @@ public class BookmarkDBAdapter {
 		newValues.put(LabelColumn.NAME, label.getName());
 
 		long newId = db.update(Table.LABEL, newValues, "_id=?", new String []{String.valueOf(label.getId())});
-		LabelDto newLabel = getLabelDto(newId);
-		return newLabel;
+		return getLabelDto(newId);
 	}
 
 	public boolean removeBookmarkLabelJoin(BookmarkDto bookmark, LabelDto label) {
@@ -117,7 +110,7 @@ public class BookmarkDBAdapter {
 	}
 
 	public List<BookmarkDto> getAllBookmarks() {
-		List<BookmarkDto> allBookmarks = new ArrayList<BookmarkDto>();
+		List<BookmarkDto> allBookmarks = new ArrayList<>();
 		Cursor c = db.query(BookmarkQuery.TABLE, BookmarkQuery.COLUMNS, null, null, null, null, null);
 		try {
 			if (c.moveToFirst()) {
@@ -136,8 +129,7 @@ public class BookmarkDBAdapter {
 
 	public List<BookmarkDto> getBookmarksInBook(BibleBook book) {
 		Log.d(TAG, "about to getBookmarksInPassage:"+book.getOSIS());
-		List<BookmarkDto> bookmarkList = new ArrayList<BookmarkDto>();
-		//av11n TODO may need to map between different versifications here
+		List<BookmarkDto> bookmarkList = new ArrayList<>();
 		Cursor c = db.query(BookmarkQuery.TABLE, BookmarkQuery.COLUMNS, BookmarkColumn.KEY+" LIKE ?", new String []{String.valueOf(book.getOSIS()+".%")}, null, null, null);
 		try {
 			if (c.moveToFirst()) {
@@ -162,7 +154,7 @@ public class BookmarkDBAdapter {
 					 "JOIN label ON (bookmark_label.label_id = label._id) "+
 					 "WHERE label._id = ? ";
 		
-		List<BookmarkDto> allBookmarks = new ArrayList<BookmarkDto>();
+		List<BookmarkDto> allBookmarks = new ArrayList<>();
 		String[] args = new String[] {label.getId().toString()};
 		Cursor c = db.rawQuery(sql, args);
 		try {
@@ -185,7 +177,7 @@ public class BookmarkDBAdapter {
 					 " FROM bookmark "+
 					 " WHERE NOT EXISTS (SELECT * FROM bookmark_label WHERE bookmark._id = bookmark_label.bookmark_id)";
 		
-		List<BookmarkDto> bookmarks = new ArrayList<BookmarkDto>();
+		List<BookmarkDto> bookmarks = new ArrayList<>();
 		Cursor c = db.rawQuery(sql, null);
 		try {
 			if (c.moveToFirst()) {
@@ -204,7 +196,7 @@ public class BookmarkDBAdapter {
 
 
 	public List<LabelDto> getAllLabels() {
-		List<LabelDto> allLabels = new ArrayList<LabelDto>();
+		List<LabelDto> allLabels = new ArrayList<>();
 		Cursor c = db.query(LabelQuery.TABLE, LabelQuery.COLUMNS, null, null, null, null, LabelColumn.NAME);
 		try {
 			if (c.moveToFirst()) {
@@ -228,7 +220,7 @@ public class BookmarkDBAdapter {
 					 "JOIN bookmark ON (bookmark_label.bookmark_id = bookmark._id) "+
 					 "WHERE bookmark._id = ?";
 		
-		List<LabelDto> labels = new ArrayList<LabelDto>();
+		List<LabelDto> labels = new ArrayList<>();
 		String[] args = new String[] {bookmark.getId().toString()};
 		Cursor c = db.rawQuery(sql, args);
 		try {
@@ -297,9 +289,6 @@ public class BookmarkDBAdapter {
 	}
 	
 	/** return Dto from current cursor position or null
-	 * @param c
-	 * @return
-	 * @throws NoSuchKeyException
 	 */
 	private BookmarkDto getBookmarkDto(Cursor c) {
 		BookmarkDto dto = new BookmarkDto();
@@ -334,9 +323,9 @@ public class BookmarkDBAdapter {
 		return dto;
 	}
 
-	public LabelDto getLabelDto(long id) {
+	private LabelDto getLabelDto(long id) {
 		LabelDto label = null;
-		
+
 		Cursor c = db.query(LabelQuery.TABLE, LabelQuery.COLUMNS, LabelColumn._ID+"=?", new String[] {String.valueOf(id)}, null, null, null);
 		try {
 			if (c.moveToFirst()) {
@@ -350,9 +339,6 @@ public class BookmarkDBAdapter {
 	}
 
 	/** return Dto from current cursor position or null
-	 * @param c
-	 * @return
-	 * @throws NoSuchKeyException
 	 */
 	private LabelDto getLabelDto(Cursor c) {
 		LabelDto dto = new LabelDto();
@@ -367,21 +353,21 @@ public class BookmarkDBAdapter {
 	}
 	
 	private interface BookmarkQuery {
-        final String TABLE = Table.BOOKMARK;
+        String TABLE = Table.BOOKMARK;
 
-		final String[] COLUMNS = new String[] {BookmarkColumn._ID, BookmarkColumn.KEY, BookmarkColumn.VERSIFICATION, BookmarkColumn.CREATED_ON};
+		String[] COLUMNS = new String[] {BookmarkColumn._ID, BookmarkColumn.KEY, BookmarkColumn.VERSIFICATION, BookmarkColumn.CREATED_ON};
 
-        final int ID = 0;
-        final int KEY = 1;
-        final int VERSIFICATION = 2;
-        final int CREATED_ON = 3;
+        int ID = 0;
+        int KEY = 1;
+        int VERSIFICATION = 2;
+        int CREATED_ON = 3;
     }
 	private interface LabelQuery {
-        final String TABLE = Table.LABEL;
+        String TABLE = Table.LABEL;
 
-		final String[] COLUMNS = new String[] {LabelColumn._ID, LabelColumn.NAME};
+		String[] COLUMNS = new String[] {LabelColumn._ID, LabelColumn.NAME};
 
-        final int ID = 0;
-        final int NAME = 1;
+        int ID = 0;
+        int NAME = 1;
     }
 }
