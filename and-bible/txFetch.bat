796cf0dc59f12d21f8d94ed1442c0c57f7e93f09
mjdenham <mjdenham@gmail.com>
2017-07-11 20:27:28 +0100

    fix txFetch.bat

--- and-bible/txFetch.bat
+++ and-bible/txFetch.bat
@@ -1,7 +1,7 @@
 rem tx status // to view
 rem tx pull --force // force download of translations ignoring file dates
 rem tx push -s // push the English source file to Transifex 
-tx pull
+tx-64 pull
 rem Java uses some legacy language codes for Hebrew and Indonesian 
-copy res\values-he\strings.xml res\values-iw
-copy res\values-id\strings.xml res\values-in
+copy app\src\main\res\values-he\strings.xml app\src\main\res\values-iw
+copy app\src\main\res\values-id\strings.xml app\src\main\res\values-in
