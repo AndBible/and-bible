f09e0e68ce165cf31607ae6820617da59cd9b0df
Martin <mjdenham@gmail.com>
2018-04-21 19:01:46 +0100

    Revert "Display date bookmark was added in the bookmark list "

--- and-bible/app/src/main/java/net/bible/android/view/activity/bookmark/BookmarkItemAdapter.java
+++ and-bible/app/src/main/java/net/bible/android/view/activity/bookmark/BookmarkItemAdapter.java
@@ -3,7 +3,6 @@ package net.bible.android.view.activity.bookmark;
 import android.content.Context;
 import android.graphics.Color;
 import android.os.Build;
-import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,7 +12,7 @@ import android.widget.ArrayAdapter;
 import net.bible.android.activity.R;
 import net.bible.android.control.bookmark.BookmarkControl;
 import net.bible.android.view.activity.base.ListActionModeHelper;
-import net.bible.android.view.util.widget.TwoLine2TitleListItem;
+import net.bible.android.view.util.widget.TwoLineListItem;
 import net.bible.service.common.CommonUtils;
 import net.bible.service.db.bookmark.BookmarkDto;
 
@@ -49,24 +48,18 @@ public class BookmarkItemAdapter extends ArrayAdapter<BookmarkDto> {
 		BookmarkDto item = getItem(position);
 
 		// Pick up the TwoLineListItem defined in the xml file
-		TwoLine2TitleListItem view;
+		TwoLineListItem view;
 		if (convertView == null) {
 			LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-			view = (TwoLine2TitleListItem) inflater.inflate(resource, parent, false);
+			view = (TwoLineListItem) inflater.inflate(resource, parent, false);
 		} else {
-			view = (TwoLine2TitleListItem) convertView;
+			view = (TwoLineListItem) convertView;
 		}
 
 		// Set value for the first text field
 		if (view.getText1() != null) {
 			String key = bookmarkControl.getBookmarkVerseKey(item);
 			view.getText1().setText(key);
-		}
-
-		// Set value for the date text field
-		if (view.getText3() != null) {
-			String sDt =  DateFormat.format("yyyy-MM-dd hh:mm", item.getCreatedOn()).toString();
-			view.getText3().setText(sDt);
 		}
 
 		// set value for the second text field
