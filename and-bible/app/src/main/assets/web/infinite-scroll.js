37f08ba4006a996536f367c5842466d4f6fce011
MartinDenham <martin.denham@faithlife.com>
2018-05-18 21:09:41 +0100

    improve upscroll positioning after insert above

--- and-bible/app/src/main/assets/web/infinite-scroll.js
+++ and-bible/app/src/main/assets/web/infinite-scroll.js
@@ -7,22 +7,27 @@
  */
 (function($) {
     $.fn.infiniScroll = function(fnLoadTextAtTop, fnLoadTextAtEnd, initialId, minId, maxId, insertAfterAtTop, insertBeforeAtBottom) {
-        var TRIM = true;
-        var MAX_PAGES = 5;
-        var MARGIN = 200;
+        // up is very hard when still scrolling so make the margin tiny to cause scroll to stop before pre-filling up
+        var UP_MARGIN = 2;
+        var DOWN_MARGIN = 200;
         var currentPos = scrollPosition();
 
 		var topId = initialId;
 		var endId = initialId;
+
+        var lastAddMoreTime = 0;
+        var addMoreAtTopOnTouchUp = false;
 
 		var scrollHandler = function() {
             previousPos = currentPos;
             currentPos = scrollPosition();
             var scrollingUp = currentPos < previousPos;
             var scrollingDown = currentPos > previousPos;
-            if (scrollingDown && currentPos >= ($('#bottomOfBibleText').offset().top - $(window).height()) - MARGIN) {
+            if (scrollingDown && currentPos >= ($('#bottomOfBibleText').offset().top - $(window).height()) - DOWN_MARGIN && Date.now()>lastAddMoreTime+1000) {
+                lastAddMoreTime = Date.now()
                 addMoreAtEnd();
-            } else if (scrollingUp && currentPos < MARGIN) {
+            } else if (scrollingUp && currentPos < UP_MARGIN && Date.now()>lastAddMoreTime+1000) {
+                lastAddMoreTime = Date.now()
                 addMoreAtTop();
             }
             currentPos = scrollPosition();
@@ -31,13 +36,16 @@
 		// Could add start() and stop() methods
 		$(window).scroll(scrollHandler);
 		//$(window).unbind("scroll", scrollHandler);
+    	 window.addEventListener('touchstart', touchstartListener, false);
+         window.addEventListener('touchend', touchendListener, false);
 
         function addMoreAtEnd() {
-			if (endId<maxId && document.getElementById("stillLoadingId")==null) {
-				var id = ++endId
+			if (endId<maxId && !stillLoading) {
+			    stillLoading = true;
+				var id = ++endId;
 				var textId = 'insertedText' + id;
 				// place marker for text which may take longer to load
-				var placeMarker = '<div id="' + textId + '" class="page_section"><p id="stillLoadingId">&nbsp;</p></div>'
+				var placeMarker = '<div id="' + textId + '" class="page_section">&nbsp;</div>';
 				$(insertBeforeAtBottom).before(placeMarker);
 
 				fnLoadTextAtEnd(id, textId);
@@ -45,11 +53,15 @@
         }
 
         function addMoreAtTop() {
-			if (topId>minId && document.getElementById("stillLoadingId")==null) {
-				var id = --topId
+            if (touchDown) {
+                // adding at top is tricky and if the user is stil holding there seems no way to set the scroll position after insert
+                addMoreAtTopOnTouchUp = true;
+            } else if (topId>minId && !stillLoading) {
+			    stillLoading = true;
+			    var id = --topId;
 				var textId = 'insertedText' + id;
 				// place marker for text which may take longer to load
-				var placeMarker = '<div id="' + textId + '" class="page_section"><p id="stillLoadingId" style="height: 100px">&nbsp;</p></div>';
+				var placeMarker = '<div id="' + textId + '" class="page_section">&nbsp;</div>';
 				insertAtTop($(insertAfterAtTop), placeMarker);
 
 				fnLoadTextAtTop(id, textId);
@@ -63,8 +75,31 @@
             var adjustedPosition =  currentPos + changeInHeight;
             setScrollPosition(adjustedPosition);
         }
+
+        function touchstartListener(event){
+            touchDown = true;
+        }
+        function touchendListener(event){
+            touchDown = false;
+            if (textToBeInsertedAtTop && idToInsertTextAt) {
+                var text = textToBeInsertedAtTop;
+                var id = idToInsertTextAt;
+                textToBeInsertedAtTop = null;
+                idToInsertTextAt = null;
+                insertThisTextAtTop(id, text);
+            }
+            if (addMoreAtTopOnTouchUp) {
+                addMoreAtTopOnTouchUp = false;
+                addMoreAtTop()
+            }
+        }
     };
 })(jQuery);
+
+var stillLoading = false;
+var touchDown = false;
+var textToBeInsertedAtTop = null;
+var idToInsertTextAt = null;
 
 $(document).ready(function() {
     var chapterInfo = JSON.parse(window.jsInterface.getChapterInfo());
@@ -103,17 +138,23 @@ function loadTextAtEnd(chapter, textId) {
  * called from java after actual text has been retrieved to request text is inserted
  */
 function insertThisTextAtTop(textId, text) {
-    var priorHeight = bodyHeight();
-	var origPosition = scrollPosition();
+    if (touchDown) {
+        textToBeInsertedAtTop = text;
+        idToInsertTextAt = textId;
+    } else {
+        var priorHeight = bodyHeight();
+        var origPosition = scrollPosition();
 
-    var $divToInsertInto = $('#' + textId);
-    $divToInsertInto.html(text);
+        var $divToInsertInto = $('#' + textId);
+        $divToInsertInto.html(text);
 
-    // do no try to get scrollPosition here becasue it has not settled
-    var adjustedTop = origPosition - priorHeight + bodyHeight();
-    setScrollPosition(adjustedTop);
+        // do no try to get scrollPosition here becasue it has not settled
+        var adjustedTop = origPosition - priorHeight + bodyHeight();
+        setScrollPosition(adjustedTop);
 
-    registerVersePositions();
+        registerVersePositions();
+        stillLoading = false;
+    }
 }
 
 function insertThisTextAtEnd(textId, text) {
@@ -121,4 +162,5 @@ function insertThisTextAtEnd(textId, text) {
     $('#' + textId).html(text);
 
     registerVersePositions();
+    stillLoading = false;
 }
