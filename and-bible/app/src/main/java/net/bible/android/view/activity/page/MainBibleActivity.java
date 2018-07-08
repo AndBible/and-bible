5a1a6bccf1f2347974be325f2ccc2857d085ff82
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2018-07-08 15:09:15 +0300

    Ask permission to write backup

--- and-bible/app/src/main/java/net/bible/android/view/activity/page/MainBibleActivity.java
+++ and-bible/app/src/main/java/net/bible/android/view/activity/page/MainBibleActivity.java
@@ -2,8 +2,10 @@ package net.bible.android.view.activity.page;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.view.ActionMode;
 import android.util.Log;
@@ -33,6 +35,7 @@ import net.bible.android.control.search.SearchControl;
 import net.bible.android.view.activity.DaggerMainBibleActivityComponent;
 import net.bible.android.view.activity.MainBibleActivityModule;
 import net.bible.android.view.activity.base.CustomTitlebarActivityBase;
+import net.bible.android.view.activity.base.Dialogs;
 import net.bible.android.view.activity.page.actionbar.BibleActionBarManager;
 import net.bible.android.view.activity.page.actionmode.VerseActionModeMediator;
 import net.bible.android.view.activity.page.screen.DocumentViewManager;
@@ -249,9 +252,21 @@ public class MainBibleActivity extends CustomTitlebarActivityBase implements Ver
 		} else if (mainMenuCommandHandler.isDocumentChanged(requestCode)) {
 			updateActionBarButtons();
 		}
-		else if(requestCode == BACKUP_REQUEST) {
-			backupControl.backupDatabase();
+
+	}
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+		if(requestCode == BACKUP_REQUEST) {
+			if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+				backupControl.backupDatabase();
+			}
+			else {
+				Dialogs.getInstance().showMsg(R.string.error_occurred);
+			}
 		}
+
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 	}
 
 	@Override
