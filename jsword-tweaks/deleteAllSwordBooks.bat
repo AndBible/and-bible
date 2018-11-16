250ab904875e413d8a90a9399bfee8d759503fd3
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2018-11-16 20:53:13 +0200

    Move stuff to other repositories (jsword-tweaks, ReadingPlanCreator)

--- jsword-tweaks/deleteAllSwordBooks.bat
+++
@@ -1,3 +0,0 @@
-del "C:\Program Files\CrossWire\The SWORD Project\mods.d\*.*" /S /Q
-rmdir "C:\Program Files\CrossWire\The SWORD Project\modules" /S /Q
-rmdir "C:\Documents and Settings\denha1m\Application Data\JSword\lucene\Sword" /S /Q
