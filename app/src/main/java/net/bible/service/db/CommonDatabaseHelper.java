5c5babcfb780766698262b1d8c88fe8ebcf10ea5
jsmitza <gitlab.jsmit@neverbox.com>
2018-12-27 16:42:20 +0200

    Fixed conversion number and other review requests

--- app/src/main/java/net/bible/service/db/CommonDatabaseHelper.java
+++ app/src/main/java/net/bible/service/db/CommonDatabaseHelper.java
@@ -31,7 +31,7 @@ import net.bible.service.db.mynote.MyNoteDatabaseDefinition;
 /**
  * Oversee database creation and upgrade based on version
  * There is a single And Bible database but creation and upgrade is implemented by the different db modules
- *  
+ *
  * @author Martin Denham [mjdenham at gmail dot com]
  */
 public class CommonDatabaseHelper extends SQLiteOpenHelper {
@@ -40,35 +40,35 @@ public class CommonDatabaseHelper extends SQLiteOpenHelper {
 	public static final String DATABASE_NAME = "andBibleDatabase.db";
 
 	private static CommonDatabaseHelper sSingleton = null;
-	
-    public static synchronized CommonDatabaseHelper getInstance() {
-        if (sSingleton == null) {
-            sSingleton = new CommonDatabaseHelper(BibleApplication.getApplication().getApplicationContext());
-        }
-        return sSingleton;
-    }
 
-    public static void reset() {
-    	sSingleton = null;
+	public static synchronized CommonDatabaseHelper getInstance() {
+		if (sSingleton == null) {
+			sSingleton = new CommonDatabaseHelper(BibleApplication.getApplication().getApplicationContext());
+		}
+		return sSingleton;
 	}
 
-    /**
-     * Private constructor, callers except unit tests should obtain an instance through
-     * {@link #getInstance()} instead.
-     */
-    CommonDatabaseHelper(Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
-    }
-	
+	public static void reset() {
+		sSingleton = null;
+	}
+
+	/**
+	 * Private constructor, callers except unit tests should obtain an instance through
+	 * {@link #getInstance()} instead.
+	 */
+	CommonDatabaseHelper(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+
 	/** Called when no database exists in disk and the helper class needs
-     *  to create a new one. 
-     */
+	 *  to create a new one.
+	 */
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		BookmarkDatabaseDefinition.getInstance().onCreate(db);
 		MyNoteDatabaseDefinition.getInstance().onCreate(db);
 	}
-	
+
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		Log.i(TAG, "Upgrading DB from version " + oldVersion + " to " + newVersion);
@@ -94,10 +94,10 @@ public class CommonDatabaseHelper extends SQLiteOpenHelper {
 				BookmarkDatabaseDefinition.getInstance().upgradeToVersion5(db);
 				oldVersion += 1;
 			}
-            if (oldVersion == 5) {
-                BookmarkDatabaseDefinition.getInstance().upgradeToVersion5(db);
-                oldVersion += 1;
-            }
+			if (oldVersion == 5) {
+				BookmarkDatabaseDefinition.getInstance().upgradeToVersion6(db);
+				oldVersion += 1;
+			}
 		} catch (SQLiteException e) {
 			Log.e(TAG, "onUpgrade: SQLiteException. " + e);
 //TODO allow complete recreation if error - too scared to do this!
