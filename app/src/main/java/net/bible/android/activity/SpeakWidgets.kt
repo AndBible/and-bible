9ea28eaf7f9c3ed321055137f231e91e21dd0f0b
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2018-12-31 20:50:55 +0200

    Add indicator about repeat mode

--- app/src/main/java/net/bible/android/activity/SpeakWidgets.kt
+++ app/src/main/java/net/bible/android/activity/SpeakWidgets.kt
@@ -191,7 +191,8 @@ class SpeakWidgetManager {
 
         for (b in bookmarkControl.getBookmarksWithLabel(labelDto).sortedWith(
                 Comparator<BookmarkDto> { o1, o2 -> o1.verseRange.start.compareTo(o2.verseRange.start) })) {
-            addButton("${b.verseRange.start.name} (${b.playbackSettings?.bookId?:"?"})", b.verseRange.start.osisRef)
+            val repeatSymbol = if(b.playbackSettings?.verseRange != null) "\t\uD83D\uDD01" else ""
+            addButton("${b.verseRange.start.name} (${b.playbackSettings?.bookId?:"?"}$repeatSymbol)", b.verseRange.start.osisRef)
             Log.d(TAG, "Added button for $b")
         }
         views.setViewVisibility(R.id.helptext, if (bookmarksAdded) View.GONE else View.VISIBLE)
