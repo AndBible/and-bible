2e15fd67ebedf70f653738286b8e840eda985ff0
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2019-01-12 11:01:17 +0200

    Improve fullcsreen mode (sticky immersive)

--- app/src/main/java/net/bible/android/view/activity/page/MainBibleActivity.kt
+++ app/src/main/java/net/bible/android/view/activity/page/MainBibleActivity.kt
@@ -451,7 +451,7 @@ class MainBibleActivity : CustomTitlebarActivityBase(), VerseActionModeMediator.
                 View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 // Set the content to appear under the system bars so that the
                 // content doesn't resize when the system bars hide and show.
-                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                //or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 // Hide the nav bar and status bar
@@ -462,9 +462,10 @@ class MainBibleActivity : CustomTitlebarActivityBase(), VerseActionModeMediator.
     // Shows the system bars by removing all the flags
     // except for the ones that make the content appear under the system bars.
     private fun showSystemUI() {
+        //window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
         window.decorView.systemUiVisibility = (
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                //View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
     }
 
