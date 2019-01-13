28d0f672169656a2b80421e89eb37f7741c69138
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2019-01-14 00:12:14 +0200

    Clean up comments

--- app/src/main/java/net/bible/android/view/activity/page/MainBibleActivity.kt
+++ app/src/main/java/net/bible/android/view/activity/page/MainBibleActivity.kt
@@ -480,21 +480,15 @@ class MainBibleActivity : CustomTitlebarActivityBase(), VerseActionModeMediator.
         window.decorView.systemUiVisibility = if(isPortrait) (
 
                 View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                // Set the content to appear under the system bars so that the
-                // content doesn't resize when the system bars hide and show.
                 or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                // Hide the nav bar and status bar
-                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                //or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 or View.SYSTEM_UI_FLAG_FULLSCREEN)
         else(
                 View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                // Set the content to appear under the system bars so that the
-                // content doesn't resize when the system bars hide and show.
-                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                //or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                // Hide the nav bar and status bar
                 or View.SYSTEM_UI_FLAG_FULLSCREEN)
     }
 
@@ -502,7 +496,7 @@ class MainBibleActivity : CustomTitlebarActivityBase(), VerseActionModeMediator.
         window.decorView.systemUiVisibility = if (isPortrait) {
             (View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
             or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    //or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            //or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
             )
         }
         else View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
