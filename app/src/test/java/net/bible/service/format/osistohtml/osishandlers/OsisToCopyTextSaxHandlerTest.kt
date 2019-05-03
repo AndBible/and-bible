3b2f21db0b11208581dfd3aa53a1028eaf1429de
timbze <tim.bze@gmail.com>
2019-05-03 05:37:17 -0600

    Test code improvements. Added 1 more test

--- app/src/test/java/net/bible/service/format/osistohtml/osishandlers/OsisToCopyTextSaxHandlerTest.kt
+++ app/src/test/java/net/bible/service/format/osistohtml/osishandlers/OsisToCopyTextSaxHandlerTest.kt
@@ -9,84 +9,111 @@ import org.xml.sax.helpers.AttributesImpl
 
 class OsisToCopyTextSaxHandlerTest {
 
-    private var osisToCopyTextSaxHandlerSingleVerse: OsisToCopyTextSaxHandler? = null
-    private var osisToCopyTextSaxHandlerMultipleVerses: OsisToCopyTextSaxHandler? = null
+    lateinit var osisToCopyTextSaxHandlerVerseNumbersOff: OsisToCopyTextSaxHandler
+    lateinit var osisToCopyTextSaxHandlerVerseNumbersOn: OsisToCopyTextSaxHandler
 
     @Before
     @Throws(Exception::class)
     fun setUp() {
-        osisToCopyTextSaxHandlerSingleVerse = OsisToCopyTextSaxHandler(false)
-        osisToCopyTextSaxHandlerMultipleVerses = OsisToCopyTextSaxHandler(true)
+        osisToCopyTextSaxHandlerVerseNumbersOff = OsisToCopyTextSaxHandler(false)
+        osisToCopyTextSaxHandlerVerseNumbersOn = OsisToCopyTextSaxHandler(true)
 
-        osisToCopyTextSaxHandlerSingleVerse!!.startDocument()
-        osisToCopyTextSaxHandlerMultipleVerses!!.startDocument()
+        osisToCopyTextSaxHandlerVerseNumbersOff.startDocument()
+        osisToCopyTextSaxHandlerVerseNumbersOn.startDocument()
     }
 
     @Test
     @Throws(Exception::class)
     fun testSingleVerse() {
-        osisToCopyTextSaxHandlerSingleVerse!!.startElement(null, null, "chapter",
+        osisToCopyTextSaxHandlerVerseNumbersOff.startElement(null, null, "chapter",
             AttributesImpl().apply { addAttribute("","osisID","chapter",null,"Rom.8") }
         )
-        osisToCopyTextSaxHandlerSingleVerse!!.startElement(null, null, "verse",
+        osisToCopyTextSaxHandlerVerseNumbersOff.startElement(null, null, "verse",
             AttributesImpl().apply { addAttribute("","osisID","verse",null,"Rom.8.28") }
         )
-        osisToCopyTextSaxHandlerSingleVerse!!.characters("And we know that all things...".toCharArray(), 0, 30)
-        osisToCopyTextSaxHandlerSingleVerse!!.characters(" ".toCharArray(), 0, 1)
-        osisToCopyTextSaxHandlerSingleVerse!!.endElement(null, null, "verse")
+        osisToCopyTextSaxHandlerVerseNumbersOff.characters("And we know that all things...".toCharArray(), 0, 30)
+        osisToCopyTextSaxHandlerVerseNumbersOff.characters(" ".toCharArray(), 0, 1)
+        osisToCopyTextSaxHandlerVerseNumbersOff.endElement(null, null, "verse")
 
-        assertThat(osisToCopyTextSaxHandlerSingleVerse!!.writer.html, equalTo("And we know that all things... "))
+        assertThat(osisToCopyTextSaxHandlerVerseNumbersOff.writer.html, equalTo("And we know that all things... "))
     }
 
     @Test
     @Throws(Exception::class)
     fun testMultipleVerses() {
-        osisToCopyTextSaxHandlerMultipleVerses!!.startElement(null, null, "chapter",
+        osisToCopyTextSaxHandlerVerseNumbersOn.startElement(null, null, "chapter",
             AttributesImpl().apply { addAttribute("","osisID","chapter",null,"Rom.8") }
         )
-        osisToCopyTextSaxHandlerMultipleVerses!!.startElement(null, null, "verse",
+        osisToCopyTextSaxHandlerVerseNumbersOn.startElement(null, null, "verse",
             AttributesImpl().apply { addAttribute("","osisID","verse",null,"Rom.8.28") }
         )
-        osisToCopyTextSaxHandlerMultipleVerses!!.characters("And we know that all things...".toCharArray(), 0, 30)
-        osisToCopyTextSaxHandlerMultipleVerses!!.characters(" ".toCharArray(), 0, 1)
-        osisToCopyTextSaxHandlerMultipleVerses!!.endElement(null, null, "verse")
-        osisToCopyTextSaxHandlerMultipleVerses!!.startElement(null, null, "reference", null)
-        osisToCopyTextSaxHandlerMultipleVerses!!.startElement(null, null, "verse",
+        osisToCopyTextSaxHandlerVerseNumbersOn.characters("And we know that all things...".toCharArray(), 0, 30)
+        osisToCopyTextSaxHandlerVerseNumbersOn.characters(" ".toCharArray(), 0, 1)
+        osisToCopyTextSaxHandlerVerseNumbersOn.endElement(null, null, "verse")
+        osisToCopyTextSaxHandlerVerseNumbersOn.startElement(null, null, "reference", null)
+        osisToCopyTextSaxHandlerVerseNumbersOn.startElement(null, null, "verse",
             AttributesImpl().apply { addAttribute("","osisID","verse",null,"Rom.8.29") }
         )
-        osisToCopyTextSaxHandlerMultipleVerses!!.characters("For whom he did foreknow".toCharArray(), 0, 24)
-        osisToCopyTextSaxHandlerMultipleVerses!!.endElement(null, null, "verse")
-        osisToCopyTextSaxHandlerMultipleVerses!!.startElement(null, null, "verse",
+        osisToCopyTextSaxHandlerVerseNumbersOn.characters("For whom he did foreknow".toCharArray(), 0, 24)
+        osisToCopyTextSaxHandlerVerseNumbersOn.endElement(null, null, "verse")
+        osisToCopyTextSaxHandlerVerseNumbersOn.startElement(null, null, "verse",
             AttributesImpl().apply { addAttribute("","osisID","verse",null,"Rom.8.30") }
         )
-        osisToCopyTextSaxHandlerMultipleVerses!!.characters("Moreover whom he did predestinate".toCharArray(), 0, 33)
-        osisToCopyTextSaxHandlerMultipleVerses!!.endElement(null, null, "verse")
+        osisToCopyTextSaxHandlerVerseNumbersOn.characters("Moreover whom he did predestinate".toCharArray(), 0, 33)
+        osisToCopyTextSaxHandlerVerseNumbersOn.endElement(null, null, "verse")
 
-        assertThat(osisToCopyTextSaxHandlerMultipleVerses!!.writer.html, equalTo("28. And we know that all things... 29. For whom he did foreknow 30. Moreover whom he did predestinate "))
+        assertThat(osisToCopyTextSaxHandlerVerseNumbersOn.writer.html, equalTo("28. And we know that all things... 29. For whom he did foreknow 30. Moreover whom he did predestinate "))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testMultipleVersesWithVerseNumbersOff() {
+        osisToCopyTextSaxHandlerVerseNumbersOff.startElement(null, null, "chapter",
+            AttributesImpl().apply { addAttribute("","osisID","chapter",null,"Rom.8") }
+        )
+        osisToCopyTextSaxHandlerVerseNumbersOff.startElement(null, null, "verse",
+            AttributesImpl().apply { addAttribute("","osisID","verse",null,"Rom.8.28") }
+        )
+        osisToCopyTextSaxHandlerVerseNumbersOff.characters("And we know that all things...".toCharArray(), 0, 30)
+        osisToCopyTextSaxHandlerVerseNumbersOff.characters(" ".toCharArray(), 0, 1)
+        osisToCopyTextSaxHandlerVerseNumbersOff.endElement(null, null, "verse")
+        osisToCopyTextSaxHandlerVerseNumbersOff.startElement(null, null, "reference", null)
+        osisToCopyTextSaxHandlerVerseNumbersOff.startElement(null, null, "verse",
+            AttributesImpl().apply { addAttribute("","osisID","verse",null,"Rom.8.29") }
+        )
+        osisToCopyTextSaxHandlerVerseNumbersOff.characters("For whom he did foreknow".toCharArray(), 0, 24)
+        osisToCopyTextSaxHandlerVerseNumbersOff.endElement(null, null, "verse")
+        osisToCopyTextSaxHandlerVerseNumbersOff.startElement(null, null, "verse",
+            AttributesImpl().apply { addAttribute("","osisID","verse",null,"Rom.8.30") }
+        )
+        osisToCopyTextSaxHandlerVerseNumbersOff.characters("Moreover whom he did predestinate".toCharArray(), 0, 33)
+        osisToCopyTextSaxHandlerVerseNumbersOff.endElement(null, null, "verse")
+
+        assertThat(osisToCopyTextSaxHandlerVerseNumbersOff.writer.html, equalTo("And we know that all things... For whom he did foreknow Moreover whom he did predestinate "))
     }
 
     @Test
     @Throws(Exception::class)
     fun testMultipleVersesAcrossChapter() {
-        osisToCopyTextSaxHandlerMultipleVerses!!.startElement(null, null, "verse",
+        osisToCopyTextSaxHandlerVerseNumbersOn.startElement(null, null, "verse",
             AttributesImpl().apply { addAttribute("","osisID","verse",null,"John.3.36") }
         )
-        osisToCopyTextSaxHandlerMultipleVerses!!.characters("  ".toCharArray(), 0, 2)
-        osisToCopyTextSaxHandlerMultipleVerses!!.characters("He that believeth on the Son hath everlasting life: and".toCharArray(), 0, 55)
-        osisToCopyTextSaxHandlerMultipleVerses!!.characters(" ".toCharArray(), 0, 1)
-        osisToCopyTextSaxHandlerMultipleVerses!!.characters("he that believeth not the Son shall not see life; but the wrath of God abideth on him.".toCharArray(), 0, 86)
-        osisToCopyTextSaxHandlerMultipleVerses!!.endElement(null, null, "verse")
-        osisToCopyTextSaxHandlerMultipleVerses!!.endElement(null, null, "chapter")
-        osisToCopyTextSaxHandlerMultipleVerses!!.startElement(null, null, "chapter",
+        osisToCopyTextSaxHandlerVerseNumbersOn.characters("  ".toCharArray(), 0, 2)
+        osisToCopyTextSaxHandlerVerseNumbersOn.characters("He that believeth on the Son hath everlasting life: and".toCharArray(), 0, 55)
+        osisToCopyTextSaxHandlerVerseNumbersOn.characters(" ".toCharArray(), 0, 1)
+        osisToCopyTextSaxHandlerVerseNumbersOn.characters("he that believeth not the Son shall not see life; but the wrath of God abideth on him.".toCharArray(), 0, 86)
+        osisToCopyTextSaxHandlerVerseNumbersOn.endElement(null, null, "verse")
+        osisToCopyTextSaxHandlerVerseNumbersOn.endElement(null, null, "chapter")
+        osisToCopyTextSaxHandlerVerseNumbersOn.startElement(null, null, "chapter",
             AttributesImpl().apply { addAttribute("","osisID","chapter",null,"John.4") }
         )
-        osisToCopyTextSaxHandlerMultipleVerses!!.startElement(null, null, "verse",
+        osisToCopyTextSaxHandlerVerseNumbersOn.startElement(null, null, "verse",
             AttributesImpl().apply { addAttribute("","osisID","verse",null,"John.4.1") }
         )
-        osisToCopyTextSaxHandlerMultipleVerses!!.characters("When therefore the Lord knew...".toCharArray(), 0, 31)
-        osisToCopyTextSaxHandlerMultipleVerses!!.endElement(null, null, "verse")
+        osisToCopyTextSaxHandlerVerseNumbersOn.characters("When therefore the Lord knew...".toCharArray(), 0, 31)
+        osisToCopyTextSaxHandlerVerseNumbersOn.endElement(null, null, "verse")
 
-        assertThat(osisToCopyTextSaxHandlerMultipleVerses!!.writer.html, equalTo("36. He that believeth on the Son hath everlasting life: and he that believeth not the Son shall not see life; but the wrath of God abideth on him. 4:1. When therefore the Lord knew... "))
+        assertThat(osisToCopyTextSaxHandlerVerseNumbersOn.writer.html, equalTo("36. He that believeth on the Son hath everlasting life: and he that believeth not the Son shall not see life; but the wrath of God abideth on him. 4:1. When therefore the Lord knew... "))
     }
 
 }
