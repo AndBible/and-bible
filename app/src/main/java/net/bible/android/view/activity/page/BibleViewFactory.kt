b417eff4a8db15cb340ff8112b057ed093db9f1e
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2019-10-17 18:28:56 +0300

    Kotlinize

---
+++ app/src/main/java/net/bible/android/view/activity/page/BibleViewFactory.kt
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2018 Martin Denham, Tuomas Airaksinen and the And Bible contributors.
+ *
+ * This file is part of And Bible (http://github.com/AndBible/and-bible).
+ *
+ * And Bible is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * And Bible is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with And Bible.
+ * If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+package net.bible.android.view.activity.page
+
+import net.bible.android.control.bookmark.BookmarkControl
+import net.bible.android.control.link.LinkControl
+import net.bible.android.control.mynote.MyNoteControl
+import net.bible.android.control.page.PageControl
+import net.bible.android.control.page.PageTiltScrollControlFactory
+import net.bible.android.control.page.window.ActiveWindowPageManagerProvider
+import net.bible.android.control.page.window.Window
+import net.bible.android.control.page.window.WindowControl
+import net.bible.android.view.activity.MainBibleActivityScope
+import net.bible.android.view.activity.page.actionmode.VerseActionModeMediator
+import net.bible.android.view.activity.page.actionmode.VerseMenuCommandHandler
+import java.lang.ref.WeakReference
+import java.util.WeakHashMap
+
+import javax.inject.Inject
+
+/**
+ * Build a new BibleView WebView for a Window
+ *
+ * @author Martin Denham [mjdenham at gmail dot com]
+ */
+@MainBibleActivityScope
+class BibleViewFactory @Inject
+constructor(private val mainBibleActivity: MainBibleActivity, private val pageControl: PageControl, private val pageTiltScrollControlFactory: PageTiltScrollControlFactory, private val windowControl: WindowControl, private val bibleKeyHandler: BibleKeyHandler, private val linkControl: LinkControl, private val bookmarkControl: BookmarkControl, private val myNoteControl: MyNoteControl, private val activeWindowPageManagerProvider: ActiveWindowPageManagerProvider) {
+
+    private val screenBibleViewMap: MutableMap<Window, BibleView>
+
+    init {
+
+        screenBibleViewMap = WeakHashMap()
+    }
+
+    fun createBibleView(window: Window): BibleView {
+        var bibleView = screenBibleViewMap[window]
+        if (bibleView == null) {
+            val pageTiltScrollControl = pageTiltScrollControlFactory.getPageTiltScrollControl(window)
+            bibleView = BibleView(this.mainBibleActivity, WeakReference(window), windowControl, bibleKeyHandler, pageControl, pageTiltScrollControl, linkControl)
+
+            val bibleViewVerseActionModeMediator = VerseActionModeMediator(mainBibleActivity, bibleView, pageControl, VerseMenuCommandHandler(mainBibleActivity, pageControl, bookmarkControl, myNoteControl), bookmarkControl)
+
+            val bibleInfiniteScrollPopulator = BibleInfiniteScrollPopulator(bibleView, window.pageManager)
+
+            val verseCalculator = VerseCalculator()
+            val bibleJavascriptInterface = BibleJavascriptInterface(bibleViewVerseActionModeMediator, windowControl, verseCalculator, window.pageManager, bibleInfiniteScrollPopulator, WeakReference(bibleView))
+            bibleView.setBibleJavascriptInterface(bibleJavascriptInterface)
+            bibleView.id = BIBLE_WEB_VIEW_ID_BASE + window.screenNo
+            bibleView.initialise()
+
+            screenBibleViewMap[window] = bibleView
+        }
+        return bibleView
+    }
+
+    companion object {
+
+        private val BIBLE_WEB_VIEW_ID_BASE = 990
+    }
+}
---
+++ app/src/main/java/net/bible/android/view/activity/page/screen/PageTiltScroller.kt
@@ -0,0 +1,163 @@
+/*
+ * Copyright (c) 2018 Martin Denham, Tuomas Airaksinen and the And Bible contributors.
+ *
+ * This file is part of And Bible (http://github.com/AndBible/and-bible).
+ *
+ * And Bible is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * And Bible is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with And Bible.
+ * If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+package net.bible.android.view.activity.page.screen
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Message
+import android.util.Log
+
+import net.bible.android.control.page.PageTiltScrollControl
+import net.bible.android.control.page.PageTiltScrollControl.TiltScrollInfo
+import net.bible.android.view.activity.page.BibleView
+
+import java.lang.ref.WeakReference
+
+/** The WebView component that shows teh main bible and commentary text
+ *
+ * @author Martin Denham [mjdenham at gmail dot com]
+ */
+class PageTiltScroller(
+	private val webView: WeakReference<BibleView>,
+	private val pageTiltScrollControl: PageTiltScrollControl
+) {
+
+    private var mScrollTriggerThread: Thread? = null
+    private var mIsScrolling: Boolean = false
+    private val mScrollMsgHandler = ScrollMsgHandler(this)
+
+    private val mScrollTrigger = ScrollTrigger()
+
+    /** start or stop tilt to scroll functionality
+     */
+    fun enableTiltScroll(enable: Boolean) {
+        if (pageTiltScrollControl.enableTiltScroll(enable)) {
+            if (enable) {
+                recalculateViewingPosition()
+                kickOffScrollThread()
+            } else {
+                stopScrollThread()
+            }
+        }
+    }
+
+    /** called when user touches screen to reset home position
+     */
+    fun recalculateViewingPosition() {
+        pageTiltScrollControl.recalculateViewingPosition()
+    }
+    /**
+     * Scroll screen at a certain speed
+     */
+
+    /** start scrolling handler
+     */
+    private fun kickOffScrollThread() {
+        if (mScrollTriggerThread == null) {
+            mScrollTrigger.enable()
+            mScrollTriggerThread = Thread(mScrollTrigger)
+            mScrollTriggerThread!!.start()
+        }
+    }
+
+    /** start scrolling handler
+     */
+    private fun stopScrollThread() {
+        if (mScrollTriggerThread != null) {
+            mScrollTrigger.stop()
+            mScrollTriggerThread = null
+        }
+    }
+
+    private inner class ScrollTrigger : Runnable {
+        private var isContinue = true
+
+        internal fun enable() {
+            isContinue = true
+        }
+
+        internal fun stop() {
+            isContinue = false
+        }
+
+        override fun run() {
+            Log.d(TAG, "Tilt-Scroll loop starting")
+            while (isContinue) {
+                try {
+                    val tiltScrollInfo = pageTiltScrollControl.tiltScrollInfo
+
+                    if (tiltScrollInfo.scrollPixels != 0) {
+                        val msg = Message()
+                        val b = Bundle()
+                        b.putInt(SCROLL_PIXELS_KEY, tiltScrollInfo.scrollPixels)
+                        b.putBoolean(FORWARD_KEY, tiltScrollInfo.forward)
+                        msg.data = b
+                        mScrollMsgHandler.sendMessageAtFrontOfQueue(msg)
+                    }
+
+                    if (pageTiltScrollControl.isTiltScrollEnabled) {
+                        val delay = (if (mIsScrolling) tiltScrollInfo.delayToNextScroll else TiltScrollInfo.TIME_TO_POLL_WHEN_NOT_SCROLLING).toLong()
+                        Thread.sleep(delay)
+                    } else {
+                        isContinue = false
+                    }
+                } catch (e: Exception) {
+                    Log.v("Error", e.toString())
+                    isContinue = false
+                }
+
+            }
+            Log.d(TAG, "Tilt-Scroll loop exiting")
+        }
+    }
+
+    /** handle message requesting the bible view be scrolled up one pixel
+     */
+    private class ScrollMsgHandler internal constructor(pageTiltScroller: PageTiltScroller) : Handler() {
+        // avoid potential memory leak.  See http://stackoverflow.com/questions/11407943/this-handler-class-should-be-static-or-leaks-might-occur-incominghandler
+        private val pageTiltScrollerRef: WeakReference<PageTiltScroller>
+
+        init {
+            this.pageTiltScrollerRef = WeakReference(pageTiltScroller)
+        }
+
+        /** scroll the window 1 pixel up
+         */
+        override fun handleMessage(msg: Message) {
+            val b = msg.data
+            val scrollPixels = b.getInt(SCROLL_PIXELS_KEY, 1)
+            val forward = b.getBoolean(FORWARD_KEY, true)
+
+            val pageTiltScroller = pageTiltScrollerRef.get()
+            if (pageTiltScroller != null) {
+                pageTiltScroller.mIsScrolling = pageTiltScroller.webView.scroll(forward, scrollPixels)
+            }
+        }
+    }
+
+    companion object {
+
+        private val FORWARD_KEY = "Forward"
+
+        private val SCROLL_PIXELS_KEY = "ScrollPixels"
+
+        private val TAG = "PageTiltScroller"
+    }
+
+}
