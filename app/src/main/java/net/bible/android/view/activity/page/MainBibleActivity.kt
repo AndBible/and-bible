71dcd37fccf5df9df99bb8b5001cf9bd3f359a10
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2019-12-17 18:13:44 +0200

    Improve navigation bar colors

--- app/src/main/java/net/bible/android/view/activity/page/MainBibleActivity.kt
+++ app/src/main/java/net/bible/android/view/activity/page/MainBibleActivity.kt
@@ -871,6 +871,10 @@ class MainBibleActivity : CustomTitlebarActivityBase(), VerseActionModeMediator.
     private fun showSystemUI() {
         var uiFlags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && !ScreenSettings.isNightMode) {
+            uiFlags = uiFlags or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+        }
+
         // only need to un-hide navigation bar in portrait mode
         if (CommonUtils.isPortrait)
             uiFlags = uiFlags or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
