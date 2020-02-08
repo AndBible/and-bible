4dc59abbfdf3d97f750a5f9b97a47cc48a3300a2
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2020-02-08 14:14:57 +0200

    Fix margin size after migration

--- app/src/main/java/net/bible/android/view/activity/page/OptionsMenuItems.kt
+++ app/src/main/java/net/bible/android/view/activity/page/OptionsMenuItems.kt
@@ -275,9 +275,10 @@ class ColorPreference(settings: SettingsBundle): Preference(settings, TextDispla
 }
 
 class MarginSizePreference(settings: SettingsBundle): Preference(settings, TextDisplaySettings.Types.MARGINSIZE, requiresReload = false) {
-    private val leftVal get() = (value as WorkspaceEntities.MarginSize).marginLeft!!
-    private val rightVal get() = (value  as WorkspaceEntities.MarginSize).marginRight!!
-    private val maxWidth get() = (value  as WorkspaceEntities.MarginSize).maxWidth!!
+    private val leftVal get() = (value as WorkspaceEntities.MarginSize).marginLeft ?: defaultVal.marginLeft!!
+    private val rightVal get() = (value  as WorkspaceEntities.MarginSize).marginRight ?: defaultVal.marginRight!!
+    private val maxWidth get() = (value  as WorkspaceEntities.MarginSize).maxWidth ?: defaultVal.maxWidth!!
+    private val defaultVal = TextDisplaySettings.default.marginSize!!
     override val title: String get() = mainBibleActivity.getString(R.string.prefs_margin_size_mm_title, leftVal, rightVal, maxWidth)
     override val visible = true
     override fun openDialog(activity: Activity, onChanged: ((value: Any) -> Unit)?, onReset: (() -> Unit)?): Boolean {
