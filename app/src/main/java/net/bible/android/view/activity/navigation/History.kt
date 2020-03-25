b0004cc27ccc3a171c1feef2e86658fb318c28c6
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2020-03-25 15:32:42 +0200

    Kotlinize History

---
+++ app/src/main/java/net/bible/android/view/activity/navigation/History.kt
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) 2020 Martin Denham, Tuomas Airaksinen and the And Bible contributors.
+ *
+ * This file is part of And Bible (http://github.com/AndBible/and-bible).
+ *
+ * And Bible is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * And Bible is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with And Bible.
+ * If not, see http://www.gnu.org/licenses/.
+ *
+ */
+package net.bible.android.view.activity.navigation
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ListAdapter
+import android.widget.ListView
+import net.bible.android.activity.R
+import net.bible.android.control.page.window.WindowControl
+import net.bible.android.view.activity.base.Dialogs.Companion.instance
+import net.bible.android.view.activity.base.ListActivityBase
+import net.bible.android.view.activity.base.SharedActivityState.Companion.currentWorkspaceName
+import net.bible.service.history.HistoryItem
+import net.bible.service.history.HistoryManager
+import java.util.*
+import javax.inject.Inject
+
+/** show a history list and allow to go to history item
+ *
+ * @author Martin Denham [mjdenham at gmail dot com]
+ */
+class History : ListActivityBase() {
+    private var mHistoryItemList: List<HistoryItem>? = null
+    @Inject lateinit var historyManager: HistoryManager
+    @Inject lateinit var windowControl: WindowControl
+    override val customTheme: Boolean
+        get() = false
+
+    /** Called when the activity is first created.  */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.i(TAG, "Displaying History view")
+        setContentView(R.layout.history)
+        buildActivityComponent().inject(this)
+        listAdapter = createAdapter()
+        val name = currentWorkspaceName
+        title = getString(R.string.history_for, name, windowControl.activeWindowPosition + 1)
+        Log.d(TAG, "Finished displaying Search view")
+    }
+
+    /**
+     * Creates and returns a list adapter for the current list activity
+     * @return
+     */
+    protected fun createAdapter(): ListAdapter {
+        mHistoryItemList = historyManager.history
+        val historyTextList: MutableList<CharSequence> = ArrayList()
+        for (item in mHistoryItemList!!) {
+            historyTextList.add(item.description)
+        }
+        return ArrayAdapter(this,
+            LIST_ITEM_TYPE,
+            historyTextList)
+    }
+
+    override fun onListItemClick(l: ListView, v: View, position: Int, id: Long) {
+        try {
+            historyItemSelected(mHistoryItemList!![position])
+        } catch (e: Exception) {
+            Log.e(TAG, "Selection error", e)
+            instance.showErrorMsg(R.string.error_occurred, e)
+        }
+    }
+
+    private fun historyItemSelected(historyItem: HistoryItem) {
+        Log.i(TAG, "chose:$historyItem")
+        historyItem.revertTo()
+        doFinish()
+    }
+
+    private fun doFinish() {
+        val resultIntent = Intent()
+        setResult(Activity.RESULT_OK, resultIntent)
+        finish()
+    }
+
+    companion object {
+        private const val TAG = "History"
+        private const val LIST_ITEM_TYPE = android.R.layout.simple_list_item_1
+    }
+}
