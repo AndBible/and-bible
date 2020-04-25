22cdd0bb041d89a06f4e53bb2f206ea048ec0e7f
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2020-04-25 23:06:30 +0200

    Wrap long lines

--- app/src/main/java/net/bible/service/format/osistohtml/taghandler/NoteHandler.kt
+++ app/src/main/java/net/bible/service/format/osistohtml/taghandler/NoteHandler.kt
@@ -37,11 +37,27 @@ import java.util.*
  * <note type="study">undefiled: or, perfect, or, sincere</note>
  *
  * Cross-reference note
- * In the <note n="a" osisID="Gen.1.1!crossReference.a" osisRef="Gen.1.1" type="crossReference"><reference osisRef="Job.38.4-Job.38.7">Job 38:4-7</reference>; <reference osisRef="Ps.33.6">Ps. 33:6</reference>; <reference osisRef="Ps.136.5">136:5</reference>; <reference osisRef="Isa.42.5">Isa. 42:5</reference>; <reference osisRef="Isa.45.18">45:18</reference>; <reference osisRef="John.1.1-John.1.3">John 1:1-3</reference>; <reference osisRef="Acts.14.15">Acts 14:15</reference>; <reference osisRef="Acts.17.24">17:24</reference>; <reference osisRef="Col.1.16-Col.1.17">Col. 1:16, 17</reference>; <reference osisRef="Heb.1.10">Heb. 1:10</reference>; <reference osisRef="Heb.11.3">11:3</reference>; <reference osisRef="Rev.4.11">Rev. 4:11</reference></note>beginning
+ * In the <note n="a" osisID="Gen.1.1!crossReference.a" osisRef="Gen.1.1" type="crossReference">
+ *     <reference osisRef="Job.38.4-Job.38.7">Job 38:4-7</reference>;
+ *     <reference osisRef="Ps.33.6">Ps. 33:6</reference>;
+ *     <reference osisRef="Ps.136.5">136:5</reference>;
+ *     <reference osisRef="Isa.42.5">Isa. 42:5</reference>;
+ *     <reference osisRef="Isa.45.18">45:18</reference>;
+ *     <reference osisRef="John.1.1-John.1.3">John 1:1-3</reference>;
+ *     <reference osisRef="Acts.14.15">Acts 14:15</reference>;
+ *     <reference osisRef="Acts.17.24">17:24</reference>;
+ *     <reference osisRef="Col.1.16-Col.1.17">Col. 1:16, 17</reference>;
+ *     <reference osisRef="Heb.1.10">Heb. 1:10</reference>;
+ *     <reference osisRef="Heb.11.3">11:3</reference>;
+ *     <reference osisRef="Rev.4.11">Rev. 4:11</reference>
+ *     </note>beginning
  *
  * @author Martin Denham [mjdenham at gmail dot com]
  */
-class NoteHandler(private val parameters: OsisToHtmlParameters, private val verseInfo: VerseInfo, private val writer: HtmlTextWriter) : OsisTagHandler {
+class NoteHandler(
+        private val parameters: OsisToHtmlParameters,
+        private val verseInfo: VerseInfo,
+        private val writer: HtmlTextWriter) : OsisTagHandler {
     private var noteCount = 0
 
     //todo temporarily use a string but later switch to Map<int,String> of verse->note
@@ -70,7 +86,13 @@ class NoteHandler(private val parameters: OsisToHtmlParameters, private val vers
         val noteText = writer.tempStoreString
         if (noteText.length > 0) {
             if (!StringUtils.containsOnly(noteText, "[];()., ")) {
-                val note = Note(verseInfo.currentVerseNo, currentNoteRef, noteText, NoteType.TYPE_GENERAL, null, null)
+                val note = Note(
+                    verseInfo.currentVerseNo,
+                    currentNoteRef,
+                    noteText,
+                    NoteType.TYPE_GENERAL,
+                    null,
+                    null)
                 notesList.add(note)
             }
             // and clear the buffer
@@ -93,7 +115,13 @@ class NoteHandler(private val parameters: OsisToHtmlParameters, private val vers
 
         // record the note information to show if user requests to see notes for this verse
         if (isInNote || parameters.isAutoWrapUnwrappedRefsInNote) {
-            val note = Note(verseInfo.currentVerseNo, currentNoteRef, refText, NoteType.TYPE_REFERENCE, osisRef, parameters.documentVersification)
+            val note = Note(
+                verseInfo.currentVerseNo,
+                currentNoteRef,
+                refText,
+                NoteType.TYPE_REFERENCE,
+                osisRef,
+                parameters.documentVersification)
             notesList.add(note)
         }
     }
