a66b27c8e80a56f9571f7dfaffaaaaf8ba69ae4f
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2020-05-27 20:18:14 +0300

    Update translations

--- txFetch.sh
+++ txFetch.sh
@@ -2,7 +2,7 @@
 echo tx status // to view
 echo tx pull --force // force download of translations ignoring file dates
 echo tx push -s // push the English source file to Transifex 
-tx pull
+tx pull --all
 echo Java uses some legacy language codes for Hebrew and Indonesian 
 cp app/src/main/res/values-he/strings.xml app/src/main/res/values-iw
 cp app/src/main/res/values-id/strings.xml app/src/main/res/values-in
