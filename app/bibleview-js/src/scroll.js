8b95e138ee7d419a19039a45f8c599c6b267642d
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2020-07-10 10:13:52 +0300

    Make extra clearance gap such that it is added only if linespacing is > 1.2

--- app/bibleview-js/src/scroll.js
+++ app/bibleview-js/src/scroll.js
@@ -63,6 +63,8 @@ export function doScrolling(elementY, duration) {
     })
 }
 
+let lineSpacing = null;
+
 export async function scrollToVerse(toId, now, delta = toolbarOffset) {
     console.log("scrollToVerse", toId, now, delta);
     stopScrolling();
@@ -78,7 +80,9 @@ export async function scrollToVerse(toId, now, delta = toolbarOffset) {
         }
         console.log("Scrolling to", toElement, toElement.offsetTop - delta);
         const lineHeight = parseFloat(window.getComputedStyle(toElement).getPropertyValue('line-height'));
-        delta += lineHeight*0.20;
+        if(lineSpacing && lineSpacing > 1.2) {
+            delta += lineHeight * 0.20;
+        }
         if(now===true) {
             window.scrollTo(0, toElement.offsetTop - delta);
         }
@@ -88,14 +92,14 @@ export async function scrollToVerse(toId, now, delta = toolbarOffset) {
     }
 }
 
-export function setDisplaySettings({marginLeft, marginRight, maxWidth, textColor, noiseOpacity, lineSpacing, justifyText, hyphenation} = {}, doNotReCalc = false) {
-
+export function setDisplaySettings({marginLeft, marginRight, maxWidth, textColor, noiseOpacity, lineSpacing: lineSpacing_, justifyText, hyphenation} = {}, doNotReCalc = false) {
+    lineSpacing = lineSpacing_ / 10;
     $(":root")
         .css("--max-width", `${maxWidth}mm`)
         .css("--text-color", textColor)
         .css("--hyphens", hyphenation ? "auto": "none")
         .css("--noise-opacity", noiseOpacity/100)
-        .css("--line-spacing", `${lineSpacing/10}em`)
+        .css("--line-spacing", `${lineSpacing}em`)
         .css("--text-align", justifyText? "justify" : "left");
     const content = $("#content")
 
@@ -112,7 +116,7 @@ export function setDisplaySettings({marginLeft, marginRight, maxWidth, textColor
 
     $("body")
         .css("color", textColor)
-        .css("line-height", `${lineSpacing/10}em`);
+        .css("line-height", `${lineSpacing}em`);
 
     if(!doNotReCalc) {
         registerVersePositions()
