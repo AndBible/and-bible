89e49f7268087440116a68e074f6abd2ac414faa
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2020-07-22 16:39:51 +0300

    Some changes to enable automatic screenshot capturing
    Using fastlane screengrab

---
+++ app/src/androidTest/java/net/bible/android/activity/MainBibleActivityTests.kt
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2020 Martin Denham, Tuomas Airaksinen and the And Bible contributors.
+ *
+ * This file is part of And Bible (http://github.com/AndBible/and-bible).
+ *
+ * And Bible is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * And Bible is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with And Bible.
+ * If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+package net.bible.android.activity
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import org.junit.runner.RunWith
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+import net.bible.android.view.activity.page.MainBibleActivity
+import net.bible.android.view.activity.page.MainBibleActivity.Companion.mainBibleActivity
+import org.crosswire.jsword.passage.VerseFactory
+import org.crosswire.jsword.versification.system.Versifications
+import org.junit.AfterClass
+import org.junit.BeforeClass
+import org.junit.Rule
+import org.junit.Test
+import tools.fastlane.screengrab.Screengrab
+import tools.fastlane.screengrab.cleanstatusbar.CleanStatusBar
+import tools.fastlane.screengrab.locale.LocaleTestRule
+import java.util.*
+
+/*
+    TODO:
+    - We need to have zip of modules in each Play Store language
+    - We need to have db with certain bookmarks, notes and workspaces
+    - We we might need to have certain settings (those can be set runtime too)
+    - In each locale, we need to automatically choose the local bible module, or the first local bible module
+ */
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class MainBibleActivityTests {
+    @get:Rule var activityRule = ActivityTestRule(MainBibleActivity::class.java)
+    @get:Rule var localeTestRule = LocaleTestRule()
+
+    @Test
+    fun testMainActivity() {
+        val doc = mainBibleActivity.swordDocumentFacade.bibles.find { it.language.code == Locale.getDefault().language }
+        GlobalScope.launch(Dispatchers.Main) {
+            mainBibleActivity.documentControl.currentPage.setCurrentDocumentAndKey(doc,
+                VerseFactory.fromString(Versifications.instance().getVersification("KJV"), "John.3.16"))
+        }
+
+        //mainBibleActivity.windowControl.activeWindowPageManager.currentPage.setCurrentDocument(doc)
+        Thread.sleep(1000)
+        //mainBibleActivity.windowControl.activeWindowPageManager.currentPage.key =
+        //onView(withId(R.id.speakButton)).perform(ViewActions.longClick())
+        Screengrab.screenshot("main_bible_view")
+    }
+
+    companion object {
+        @BeforeClass @JvmStatic
+        fun beforeAll() {
+            CleanStatusBar.enableWithDefaults()
+        }
+
+        @AfterClass @JvmStatic
+        fun afterAll() {
+            CleanStatusBar.disable()
+        }
+    }
+}
