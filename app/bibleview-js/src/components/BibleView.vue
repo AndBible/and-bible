cbb9aaaa6621aab804857293cbf7fdc47e1c17cf
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2020-11-05 19:07:12 +0200

    BibleView vue component drafting

--- app/bibleview-js/src/components/BibleView.vue
+++ app/bibleview-js/src/components/BibleView.vue
@@ -1,14 +1,53 @@
 <template>
   <div>
-    Test from vue <span class="test">{{test}}</span>
+     <span
+         v-for="verse in verses"
+         :id="verse|toId"
+         :key="verse|toId"
+         class="verse"
+     >
+       
+       <template v-html="verse.text"/>
+     </span>
   </div>
 </template>
 <script>
+  import "@/code"
+
   export default {
     name: "BibleView",
+    filters: {
+      toId(verse) {
+        return `${verse.chapter}.${verse.verse}`;
+      }
+    },
     data() {
       return {
-        test: "World"
+        verses: [
+          {
+            ordinal: 1,
+            chapter: 1,
+            verse: 1,
+            text: "<span>some text</span>"
+          }
+        ],
+        bookmarks: [
+          {
+            range: [1, 2], // ordinal range
+            labels: [1, 2]
+          }
+        ],
+        labelsStyles: [
+          {
+            id: 1,
+            color: "#FF0000"
+          },
+          {
+            id: 2,
+            color: "#00FF00"
+          }
+        ]
+
       }
     }
   }
