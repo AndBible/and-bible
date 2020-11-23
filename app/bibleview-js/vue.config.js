5d2979892f5cce6b453493e58a1f1b64f8663eee
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2020-11-23 20:52:59 +0200

    Change to last 2 android versions, it should be enough as webview can be updated on lollipop+

--- app/bibleview-js/vue.config.js
+++ app/bibleview-js/vue.config.js
@@ -3,6 +3,7 @@ module.exports = {
   lintOnSave: false,
   runtimeCompiler: true,
   //productionSourceMap: false,
+  transpileDependencies: ["dom-highlight-range"],
   publicPath: "",
   configureWebpack: {
     devtool: 'inline-source-map'
