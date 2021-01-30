b4b334f5a9a6301c675fd3ece7b85fedf10f0b00
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2021-01-30 10:07:32 +0200

    Update bookmark date when saving note

--- db/src/main/java/net/bible/android/database/bookmarks/BookmarksDao.kt
+++ db/src/main/java/net/bible/android/database/bookmarks/BookmarksDao.kt
@@ -131,8 +131,9 @@ interface BookmarkDao {
     fun bookmarksWithLabel(label: Label, orderBy: BookmarkSortOrder = BookmarkSortOrder.BIBLE_ORDER): List<Bookmark>
         = bookmarksWithLabel(label.id, orderBy.name)
 
-    @Query("UPDATE Bookmark SET notes=:notes WHERE id=:bookmarkId")
-    fun saveBookmarkNote(bookmarkId: Long, notes: String?)
+    @Query("""UPDATE Bookmark SET notes=:notes, lastUpdatedOn=:lastUpdatedOn WHERE id=:bookmarkId""")
+    fun saveBookmarkNote(bookmarkId: Long, notes: String?, lastUpdatedOn: Long)
+    fun saveBookmarkNote(bookmarkId: Long, notes: String?) = saveBookmarkNote(bookmarkId, notes, System.currentTimeMillis())
 
     // Labels
 
