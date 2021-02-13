aefa110835954838236cf045b6584f72362c957f
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2021-02-13 12:12:25 +0200

    Remove min-height from modal

--- app/bibleview-js/src/components/modals/Modal.vue
+++ app/bibleview-js/src/components/modals/Modal.vue
@@ -128,7 +128,7 @@ $border-radius2: $border-radius - 1.5pt;
 
 .modal-body {
   --max-height: calc(100vh - var(--top-offset) - var(--bottom-offset) - 80pt);
-  min-height: 60pt;
+  //min-height: 60pt;
   padding: 5px 5px;
   margin: 5pt 5pt;
 }
