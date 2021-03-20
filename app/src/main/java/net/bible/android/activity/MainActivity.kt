4ec9cb2a2774a911bf3032b9268d32de6bd636b3
Joshua Probert <jprobert@gmail.com>
2021-03-19 22:07:29 -0700

    Added a blank default main activity and set it to launch in AndroidManifest.xml

---
+++ app/src/main/java/net/bible/android/activity/MainActivity.kt
@@ -0,0 +1,20 @@
+package net.bible.android.activity
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+}
