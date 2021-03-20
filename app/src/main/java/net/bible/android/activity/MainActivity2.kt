f322f8ea312736bcba76f64b92a6721aac35e6bf
eanndale <eanndale@gmail,com>
2021-03-20 12:35:37 -0700

    add main activity 2

---
+++ app/src/main/java/net/bible/android/activity/MainActivity2.kt
@@ -0,0 +1,11 @@
+package net.bible.android.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
