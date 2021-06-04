f37dd1943f4e2f8e27a55b77d8ae9d617f80f2c3
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2021-06-04 19:58:00 +0300

    Revert "Cancel fix for #1110 as it looks it breaks things"
    This reverts commit e7582e9f88644af2c476bbc0a976914cf7fc5241.

--- app/src/main/java/net/bible/android/view/activity/page/screen/SplitBibleArea.kt
+++ app/src/main/java/net/bible/android/view/activity/page/screen/SplitBibleArea.kt
@@ -168,6 +168,11 @@ class SplitBibleArea: FrameLayout(mainBibleActivity) {
             updateRestoreButtons()
     }
 
+    override fun onDetachedFromWindow() {
+        removeAllFrames()
+        super.onDetachedFromWindow()
+    }
+
     private fun removeSeparators() {
         for(v in binding.biblesLinearLayout.children.filter { it is Separator }.map{it as Separator}) {
             v.frame1.removeView(v.touchDelegateView1)
