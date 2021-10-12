b24ccced17d50b55ceb945f0416ab78b52a22eec
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2021-10-12 17:38:39 +0300

    Add sk to good languages list

--- app/src/main/java/net/bible/service/common/CommonUtils.kt
+++ app/src/main/java/net/bible/service/common/CommonUtils.kt
@@ -1115,7 +1115,7 @@ object CommonUtils : CommonUtilsBase() {
 
         val goodLanguages = listOf(
             "en", "af", "my", "eo", "fi", "fr", "de", "hi", "hu", "it", "lt", "pl", "ru", "sl", "es", "uk", "zh-Hant-TW", "kk", "pt",
-            "zh-Hans-CN", "cs",
+            "zh-Hans-CN", "cs", "sk"
         )
 
         fun checkLanguage(lang: String): Boolean =
