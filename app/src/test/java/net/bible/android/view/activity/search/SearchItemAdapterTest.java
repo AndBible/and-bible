3bb5d8f784d78cda2d4cb03395ae2b81c244e6d0
Andrew Rogers <rogers.ag@gmail.com>
2021-12-06 14:55:27 +1000

    Create SearchItemAdapterTest.java

---
+++ app/src/test/java/net/bible/android/view/activity/search/SearchItemAdapterTest.java
@@ -0,0 +1,26 @@
+package net.bible.android.view.activity.search;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SearchItemAdapterTest {
+
+	@Before
+	public void setup() {
+	}
+
+	@Test
+	public void testHebrewSearch() throws Exception {
+		// Placeholder for a future test
+	}
+
+	@Test
+	public void testGreekSearch() throws Exception {
+		// Placeholder for a future test
+	}
+
+}
