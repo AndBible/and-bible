1bbf16cbd715e4e3e6dd5b393c8a02d6d0c2d4f9
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2022-01-13 17:55:52 +0200

    Fix some action bar colors

--- app/src/main/java/net/bible/android/view/activity/base/ActivityBase.kt
+++ app/src/main/java/net/bible/android/view/activity/base/ActivityBase.kt
@@ -33,7 +33,7 @@ import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import kotlinx.coroutines.CompletableDeferred
-import net.bible.android.view.activity.page.MainBibleActivity
+import net.bible.android.view.util.UiUtils.setActionBarColor
 import net.bible.android.view.util.locale.LocaleHelper
 import net.bible.service.common.CommonUtils
 import net.bible.service.device.ScreenSettings
@@ -105,6 +105,7 @@ abstract class ActivityBase : AppCompatActivity(), AndBibleActivity {
 
         // if locale is overridden then have to force title to be translated here
         LocaleHelper.translateTitle(this)
+        setActionBarColor(supportActionBar)
     }
 
     protected fun buildActivityComponent() = CommonUtils.buildActivityComponent()
