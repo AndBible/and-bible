cb069c95a1181901a21a274c3f56faa52f5098dc
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2022-05-26 16:34:04 +0300

    Fix js tests

--- app/bibleview-js/jest.config.js
+++ app/bibleview-js/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
   transform: {
-    '^.+\\.vue$': 'vue-jest',
+    '^.+\\.vue$': '@vue/vue3-jest',
     '^.+\\.xml': 'jest-raw-loader',
     '^.+\\.html': 'jest-raw-loader',
     '^.+\\.txt': 'jest-raw-loader',
