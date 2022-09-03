d93eecbb839844ccc5a5880f2c7edd68f2a9b72c
Andrew Rogers <rogers.ag@gmail.com>
2022-09-03 19:53:56 +0630

    Adjusted slightly for existing tests

--- app/src/main/java/net/bible/service/sword/SwordContentFacade.kt
+++ app/src/main/java/net/bible/service/sword/SwordContentFacade.kt
@@ -214,7 +214,7 @@ object SwordContentFacade {
         val start = startVerse.slice(0 until min(startOffset, startVerse.length))
 
         var startVerseNumber = ""
-        if (showVerseNumbers && !showReferenceAtFront) {
+        if (showVerseNumbers && !showReferenceAtFront && verseTexts.size > 1) {
             startVerseNumber = "${selection.verseRange.start.verse}. "
         }
         if (showSelectionOnly && startOffset > 0 && showEllipsis) {
