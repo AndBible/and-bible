533a7ea47f5bd848fa7bfd1376364f3a9f3e66f6
Andrew Rogers <rogers.ag@gmail.com>
2022-09-26 20:36:32 +0630

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.3.0")
+        classpath("com.android.tools.build:gradle:7.2.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("org.jetbrains.kotlin:kotlin-serialization:$kotlinVersion")
 
