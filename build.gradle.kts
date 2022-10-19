259b1cd084c1945c98cd90f622e5797405207320
Tuomas Airaksinen <tuomas.airaksinen@vrt.fi>
2022-10-19 18:13:24 +0300

    Upgrade gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.3.0")
+        classpath("com.android.tools.build:gradle:7.3.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("org.jetbrains.kotlin:kotlin-serialization:$kotlinVersion")
 
