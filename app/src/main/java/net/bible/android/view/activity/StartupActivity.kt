a8ae1b299503f78325f24c511378ae745ee6c6f2
Tuomas Airaksinen <tuomas.airaksinen@vrt.fi>
2023-02-11 16:05:21 +0200

    Improve startup startup screen instructions

--- app/src/main/java/net/bible/android/view/activity/StartupActivity.kt
+++ app/src/main/java/net/bible/android/view/activity/StartupActivity.kt
@@ -257,7 +257,7 @@ open class StartupActivity : CustomTitlebarActivityBase() {
             val myBible = getString(R.string.format_mybible)
             val mySword = getString(R.string.format_mysword)
             val formats = getString(R.string.supported_formats, "$zip, $myBible, $mySword")
-            welcomeMessage.text = "$welcome $formats"
+            welcomeMessage.text = "$welcome $formats."
             versionText.text = versionMsg
             downloadButton.setOnClickListener { doGotoDownloadActivity() }
             importButton.setOnClickListener { onLoadFromZip() }
