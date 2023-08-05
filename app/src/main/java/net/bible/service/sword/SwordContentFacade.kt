bca68da86459d3d135c9cd65680c2fcb2a1c065a
Tuomas Airaksinen <tuomas.airaksinen@vrt.fi>
2023-08-05 12:45:22 +0300

    Improve bible ref regex

--- app/src/main/java/net/bible/service/sword/SwordContentFacade.kt
+++ app/src/main/java/net/bible/service/sword/SwordContentFacade.kt
@@ -140,7 +140,7 @@ object SwordContentFacade {
 
     private const val bibleRefParseEnabled = true
 
-    private val bibleRefRe = Regex("""(\d +)?[A-Z]\w+\.? \d+:\d+([–—-]\d+(:\d+)?)?(,? ?(\d+|\d+:\d+)([–—-]\d+(:\d+)?)?)*""")
+    private val bibleRefRe = Regex("""(\d\s+)?[A-Z]\w+\.?\s+\d+:\d+([–—-]\d+(:\d+)?)?(,?\s*(\d+|\d+:\d+)([–—-]\d+(:\d+)?)?)*""")
     private fun bibleRefSplit(text: String): List<Pair<String, Boolean>> {
         val matches = bibleRefRe.findAll(text)
         val pieces = mutableListOf<Pair<String, Boolean>>()
