5af51e684aa9a5defe574598ef4c08c499812ee2
Tuomas Airaksinen <tuomas.airaksinen@vrt.fi>
2023-08-10 18:44:22 +0300

    Cleanup unused code

--- db/src/main/java/net/bible/android/database/bookmarks/SpeakSettings.kt
+++ db/src/main/java/net/bible/android/database/bookmarks/SpeakSettings.kt
@@ -50,26 +50,6 @@ object VerseRangeSerializer: KSerializer<VerseRange?> {
         return try {VerseRangeFactory.fromString(v11n, splitted[1]) } catch (e: NoSuchVerseException) {null}
     }
 }
-//@Serializer(forClass = IntRange::class)
-object IntRangeSerializer: KSerializer<IntRange?> {
-    override fun serialize(encoder: Encoder, obj: IntRange?) {
-        if(obj != null) {
-            encoder.encodeString("${obj.first}::${obj.last}")
-        }
-        else {
-            encoder.encodeNull()
-        }
-    }
-
-    override val descriptor = PrimitiveSerialDescriptor("IntRange", PrimitiveKind.STRING)
-
-    private val splitRe = Regex("""(\d+)::(\d+)""")
-    override fun deserialize(decoder: Decoder): IntRange? {
-        val str = decoder.decodeString()
-        val m = splitRe.matchEntire(str)?: return null
-        return try {m.groupValues[1].toInt()..m.groupValues[2].toInt()} catch (e: Exception) {null}
-    }
-}
 
 @Serializable
 data class PlaybackSettings (
@@ -85,10 +65,6 @@ data class PlaybackSettings (
 
     // For bible bookmarks
     @Serializable(with=VerseRangeSerializer::class) var verseRange: VerseRange? = null,
-
-    // For generic bookmarks
-    var repeatKey: String? = null,
-    @Serializable(with=IntRangeSerializer::class) var repeatOrdinalRange: IntRange? = null,
 ) {
     companion object {
 
