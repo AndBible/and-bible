889cd8295d90c453c8357e23f7a278ec6509618c
Tuomas Airaksinen <tuomas.airaksinen@vrt.fi>
2023-10-12 13:42:50 +0300

    Remove spinner (test build, #2576)

--- app/bibleview-js/src/components/BibleView.vue
+++ app/bibleview-js/src/components/BibleView.vue
@@ -323,37 +323,37 @@ $ring-thickness: calc(#{$ring-size} / 12);
 
 $ring-color: $button-grey;
 
-.lds-ring {
-  display: inline-block;
-  position: relative;
-  width: $ring-size;
-  height: $ring-size;
-
-  & div {
-    box-sizing: border-box;
-    display: block;
-    position: absolute;
-    width: $ring-size;
-    height: $ring-size;
-    margin: 8px;
-    border: $ring-thickness solid $ring-color;
-    border-radius: 50%;
-    animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
-    border-color: $ring-color transparent transparent transparent;
-
-    &:nth-child(1) {
-      animation-delay: -0.45s;
-    }
-
-    &:nth-child(2) {
-      animation-delay: -0.3s;
-    }
-
-    &:nth-child(3) {
-      animation-delay: -0.15s;
-    }
-  }
-}
+//.lds-ring {
+//  display: inline-block;
+//  position: relative;
+//  width: $ring-size;
+//  height: $ring-size;
+//
+//  & div {
+//    box-sizing: border-box;
+//    display: block;
+//    position: absolute;
+//    width: $ring-size;
+//    height: $ring-size;
+//    margin: 8px;
+//    border: $ring-thickness solid $ring-color;
+//    border-radius: 50%;
+//    animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+//    border-color: $ring-color transparent transparent transparent;
+//
+//    &:nth-child(1) {
+//      animation-delay: -0.45s;
+//    }
+//
+//    &:nth-child(2) {
+//      animation-delay: -0.3s;
+//    }
+//
+//    &:nth-child(3) {
+//      animation-delay: -0.15s;
+//    }
+//  }
+//}
 
 @keyframes lds-ring {
   0% {
