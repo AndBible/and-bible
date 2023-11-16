9bdc35e58c218200e0296ad6505e43f56115a22e
Tuomas Airaksinen <tuomas.airaksinen@vrt.fi>
2023-11-16 10:20:42 +0200

    Fix good translations date

--- app/src/main/java/net/bible/service/common/CommonUtils.kt
+++ app/src/main/java/net/bible/service/common/CommonUtils.kt
@@ -1367,7 +1367,7 @@ object CommonUtils : CommonUtilsBase() {
 
         Log.i(TAG, "Language tag $languageTag, code $languageCode")
 
-        // Transifex as of 28.10.2023
+        // Transifex as of 16.11.2023
         val goodLanguages = "en,af,fi,fr,de,it,pt-BR,ro,sk,sl,tr,kk,uk,cz,lt,yue,zh-Hans-CN,zh-Hant-TW,es,ta,cs,hu,nl".split(",")
 
         // 4.0 list:
