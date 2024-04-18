8b0b9040d57bc6d863c35986777df7b7c0ba485a
Tuomas Airaksinen <tuomas.airaksinen@vrt.fi>
2024-04-18 10:14:00 +0300

    Upgrade gradle (due to Android Studio upgrade)

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.3.1")
+        classpath("com.android.tools.build:gradle:8.3.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.21")
         classpath("org.jetbrains.kotlin:kotlin-serialization:$kotlinVersion")
 
