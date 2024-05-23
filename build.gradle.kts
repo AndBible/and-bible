0dd4475693eeca02f86219a188241d4078c9a52b
Tuomas Airaksinen <tuomas.airaksinen@vrt.fi>
2024-05-23 20:15:05 +0300

    Revert "Upgrade gradle dependencies"
    This reverts commit 3faeda301eb2ccec431b219d42141d73ec1d0692.

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,7 +42,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.4.1")
+        classpath("com.android.tools.build:gradle:8.3.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.21")
         classpath("org.jetbrains.kotlin:kotlin-serialization:$kotlinVersion")
 
