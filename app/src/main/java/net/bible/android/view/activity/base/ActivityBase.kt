dcaacdb7fce3f9d77fbd1748344297605dbdde37
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2025-07-12 12:26:38 +0300

    Enable edge-to-edge layout support in ActivityBase

--- app/src/main/java/net/bible/android/view/activity/base/ActivityBase.kt
+++ app/src/main/java/net/bible/android/view/activity/base/ActivityBase.kt
@@ -31,6 +31,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
+import androidx.activity.SystemBarStyle
+import androidx.activity.enableEdgeToEdge
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
@@ -45,6 +47,7 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
+import net.bible.android.activity.R
 import net.bible.android.view.activity.StartupActivity
 import net.bible.android.view.activity.comingFromStartupActivity
 import net.bible.android.view.activity.discrete.CalculatorActivity
@@ -93,6 +96,7 @@ abstract class ActivityBase : AppCompatActivity(), AndBibleActivity {
 
         super.onCreate(savedInstanceState)
         if (runSetupEdgeToEdge) {
+            enableEdgeToEdge()
             setupEdgeToEdge()
         }
 
