83c7b0faf6348b5452f6bb98d64cc4d8f9646abb
Andrew Rogers <rogers.ag@gmail.com>
2025-12-28 22:24:22 +0630

    Jump to verse 0 when changing chapters
    There are significant issues with this code when moving to a chapter that is already displayed or almost displayed on screen.

--- app/src/main/java/net/bible/android/view/activity/navigation/GridChoosePassageBook.kt
+++ app/src/main/java/net/bible/android/view/activity/navigation/GridChoosePassageBook.kt
@@ -248,7 +248,7 @@ class GridChoosePassageBook : CustomTitlebarActivityBase(R.menu.choose_passage_b
             // if there is only 1 chapter then no need to select chapter, but may need to select verse still
             if (!navigationControl.hasChapters(book)) {
                 if (!navigateToVerse) {
-                    val verse = Verse(v11n, book, 1, 1)
+                    val verse = Verse(v11n, book, 1, 0)
 
                     val resultIntent = Intent(this, GridChoosePassageBook::class.java)
                     resultIntent.putExtra("verse", verse.osisID)
@@ -259,7 +259,7 @@ class GridChoosePassageBook : CustomTitlebarActivityBase(R.menu.choose_passage_b
                     // select verse (only 1 chapter)
                     val myIntent = Intent(this, GridChoosePassageVerse::class.java)
                     myIntent.putExtra(GridChoosePassageBook.BOOK_NO, bibleBookNo)
-                    myIntent.putExtra(GridChoosePassageBook.CHAPTER_NO, 1)
+                    myIntent.putExtra(GridChoosePassageBook.CHAPTER_NO, 0)
                     myIntent.putExtra("navigateToVerse", navigateToVerse)
                     startActivityForResult(myIntent, 1)
                 }
