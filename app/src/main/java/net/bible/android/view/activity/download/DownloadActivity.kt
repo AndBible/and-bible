88e4165c505981a598d23723440c8e0aeb5f3199
JyriWee <jyriwee@proton.me>
2026-03-06 05:14:20 +0200

    Refine Download Documents help wording
    Fixes Copilot review feedback by making the linked help text read naturally and by keeping the custom repositories wiki URL local to DownloadActivity.

--- app/src/main/java/net/bible/android/view/activity/download/DownloadActivity.kt
+++ app/src/main/java/net/bible/android/view/activity/download/DownloadActivity.kt
@@ -529,6 +529,7 @@ open class DownloadActivity : DocumentSelectionBase(
 
     companion object {
         private const val supportedModuleFormatsWikiUrl = "https://github.com/AndBible/and-bible/wiki/Supported-3rd-party-module-formats"
+        private const val customRepositoriesWikiUrl = "https://github.com/AndBible/and-bible/wiki/Custom-repositories"
         private const val REPO_REFRESH_DATE = "repoRefreshDate"
         private const val REPO_LIST_STALE_AFTER_DAYS: Long = 1
         private const val MILLISECS_IN_DAY = 1000 * 60 * 60 * 24.toLong()
