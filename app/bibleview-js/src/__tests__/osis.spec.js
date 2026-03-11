2a4a5fa56dd10189507e47e09bd0c83a49c3c636
Tom Donahue <tomdonahue38@gmail.com>
2026-03-11 18:13:28 -0500

    fix(tests): vue rendering tests

--- app/bibleview-js/src/__tests__/osis.spec.js
+++ app/bibleview-js/src/__tests__/osis.spec.js
@@ -20,13 +20,15 @@ import OsisSegment from "@/components/documents/OsisSegment.vue";
 
 import test1Xml from "./testdata/eph.2-kjva.xml";
 import test1Result from "./testdata/eph.2-kjva-result.html";
+import crosswireXml from "./testdata/mt.5-crosswire-kjv.xml";
+import crosswireResult from "./testdata/mt.5-crosswire-kjv-result.html";
 
 
 import {useConfig} from "@/composables/config";
 import {useStrings} from "@/composables/strings";
 import {useAndroid} from "@/composables/android";
 import {useOrdinalHighlight} from "@/composables/ordinal-highlight";
-import {ref} from "vue";
+import {computed, ref} from "vue";
 import {
     androidKey,
     appSettingsKey,
@@ -37,17 +39,24 @@ import {
     modalKey,
     osisFragmentKey,
     stringsKey,
-    ordinalHighlightKey
+    ordinalHighlightKey,
+    keyboardKey,
+    scrollKey
 } from "@/types/constants";
 import AmbiguousSelection from "@/components/modals/AmbiguousSelection.vue";
 import BookmarkLabelActions from "@/components/modals/BookmarkLabelActions.vue";
 import LabelList from "@/components/LabelList.vue";
 import {useGlobalBookmarks} from "@/composables/bookmarks";
 import {useModal} from "@/composables/modal";
+import {useKeyboard} from "@/composables/keyboard";
+import {useScroll} from "@/composables/scroll";
 import { describe, it, expect } from 'vitest'
 
 window.bibleViewDebug = {}
-window.bibleView = {}
+window.bibleView = {
+    onKeyDown: () => {},
+    setEditing: () => {}
+}
 
 function verifyXmlRendering(xmlTemplate, renderedHtml) {
     const {config, appSettings, calculatedConfig} = useConfig(ref("bible"));
@@ -55,7 +64,35 @@ function verifyXmlRendering(xmlTemplate, renderedHtml) {
         bookCategory: "BIBLE",
     };
 
+    // Inject missing verseOrdinals into the XML before rendering
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(`<root>${xmlTemplate}</root>`, "text/xml");
+    const verses = xmlDoc.getElementsByTagName("verse");
+    for (let i = 0; i < verses.length; i++) {
+        if (!verses[i].getAttribute("verseOrdinal")) {
+            // Use a predictable sequential ordinal for the test
+            verses[i].setAttribute("verseOrdinal", (i + 1).toString());
+        }
+    }
+    
+    // Serialize the modified XML back to a string
+    const serializer = new XMLSerializer();
+    const root = xmlDoc.documentElement;
+    let processedXml = "";
+    for (let i = 0; i < root.childNodes.length; i++) {
+        processedXml += serializer.serializeToString(root.childNodes[i]);
+    }
+
     const android = useAndroid({bookmarks: null}, config);
+    if (android) {
+        android.onKeyDown = android.onKeyDown || (() => {});
+        android.setEditing = android.setEditing || (() => {});
+    }
+
+    const highlight = useOrdinalHighlight();
+    const scroll = useScroll(config, appSettings, calculatedConfig, highlight, ref(null));
+    const keyboard = useKeyboard(android, scroll, computed(() => 16));
+
     const provide = {
         [osisFragmentKey]: osisFragment,
         [configKey]: config,
@@ -64,19 +101,23 @@ function verifyXmlRendering(xmlTemplate, renderedHtml) {
         [footnoteCountKey]: {getFootNoteCount: () => 0},
         [androidKey]: android,
         [stringsKey]: useStrings(),
-        [ordinalHighlightKey]: useOrdinalHighlight(),
+        [ordinalHighlightKey]: highlight,
         [globalBookmarksKey]: useGlobalBookmarks(config),
         [modalKey]: useModal(android),
+        [keyboardKey]: keyboard,
+        [scrollKey]: scroll,
     };
     const components = {AmbiguousSelection, LabelList, BookmarkLabelActions};
-    const wrapper = mount(OsisSegment, {props: {osisTemplate: xmlTemplate, convert: true}, global: {provide, components}});
-    expect(wrapper.html() + "\n").toBe(renderedHtml);
+    const wrapper = mount(OsisSegment, {props: {osisTemplate: processedXml, convert: true}, global: {provide, components}});
+    const vueHtml = wrapper.html();
+    import('fs').then(fs => fs.writeFileSync('./test.html', vueHtml + '\n'));
+    expect(vueHtml + "\n").toBe(renderedHtml);
 }
 
 describe("OsisSegment.vue", () => {
-    // Skipping this now. Need to figure out how to make sure scoped css do not break our test
-    // This does not seem to work, for some reason
-    // https://runthatline.com/test-css-module-classes-in-vue-with-vitest/
-    // https://github.com/AndBible/and-bible/issues/2434
-    it.skip("Test rendering of Eph 2:8 in KJVA, #1985", () => verifyXmlRendering(test1Xml, test1Result));
+    it("Test rendering of Eph 2:8 in KJVA, #1985", () => verifyXmlRendering(test1Xml, test1Result));
+});
+
+describe("OsisSegment crosswire kjv test", () => {
+    it("Test rendering of Crosswire KJV on Mt 5", () => verifyXmlRendering(crosswireXml, crosswireResult));
 });
