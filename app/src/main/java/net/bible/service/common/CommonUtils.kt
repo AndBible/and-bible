7d1337cb30b2c99dc803eb5a02c79eca9a04199f
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2026-04-17 08:48:46 +0300

    Remove unused duplicate strings and replace help_search_text
    Remove ai_document_delete and backup_restore (unreferenced duplicates
    of 'delete' and 'restore'). Replace help_search_text with
    help_search_text2 in CommonUtils and remove the old string.

--- app/src/main/java/net/bible/service/common/CommonUtils.kt
+++ app/src/main/java/net/bible/service/common/CommonUtils.kt
@@ -1048,7 +1048,7 @@ object CommonUtils : CommonUtilsBase() {
             HelpItem(R.string.help_window_pinning_title, R.string.help_window_pinning_text, windowPinningVideo),
             HelpItem(R.string.help_bookmarks_title, R.string.help_bookmarks_text, bookmarksMyNotesPlaylist), // beta video
             HelpItem(R.string.studypads, R.string.help_studypads_text, studyPadsVideo), // beta video
-            HelpItem(R.string.help_search_title, R.string.help_search_text),
+            HelpItem(R.string.help_search_title, R.string.help_search_text2),
             HelpItem(R.string.help_workspaces_title, R.string.help_workspaces_text, workspacesVideo),
             HelpItem(R.string.help_hidden_features_title, R.string.help_hidden_features_text)
         ).run {
