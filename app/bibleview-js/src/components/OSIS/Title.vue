854b722069fd416402f95853d35bd6f1e8dbf151
Senya <senyakirilovich@gmail.com>
2026-07-25 19:40:00 +0300

    Revert outline button gating by showTitleScrollButton
    The outline button should show whenever headings exist, independent
    of the old showTitleScrollButton setting (which defaults to false).
    The dead setting will be cleaned up separately.

--- app/bibleview-js/src/components/OSIS/Title.vue
+++ app/bibleview-js/src/components/OSIS/Title.vue
@@ -17,7 +17,7 @@
 
 <template>
   <div class="title-wrapper" v-if="show">
-    <button v-if="config.showTitleScrollButton && outlineCtx?.visible" class="title-outline-btn" @click.stop="outlineCtx.open" :aria-label="strings.outline" :title="strings.outline">≡</button>
+    <button v-if="outlineCtx?.visible" class="title-outline-btn" @click.stop="outlineCtx.open" :aria-label="strings.outline" :title="strings.outline">≡</button>
     <component :is="headingTag" ref="titleEl" class="titleStyle" :class="{'skip-offset': isBibleDoc && !isCanonical, isSubTitle}" :data-ordinal="ordinal" :data-title-index="titleIndex" tabindex="0" @click="titleClicked" @keydown.enter="titleClicked" @keydown.space.prevent="titleClicked">
       <template v-if="overrideText !== null">{{ overrideText }}</template>
       <slot v-else/>
